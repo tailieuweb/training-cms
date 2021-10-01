1c6d98d7744e886ceb0168aa4521c43aab6e02dc
minhtris123 <trjkujnop@gmail.com>
2021-10-01 11:51:25 +0700

    Merge branch '2-wordpress-581-202109/2-B/master' into 2-wordpress-581-202109/2-B/5-40-Tri

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -19,6 +19,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
 		<link rel="profile" href="https://gmpg.org/xfn/11">
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 		<?php wp_head(); ?>
 
