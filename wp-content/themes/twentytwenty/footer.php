752ae446544a1d3c7c4c8e801d63a48a8135d703
nhquyen <nguyenhuuquyen2001@gmail.com>
2021-10-01 11:05:47 +0700

    update

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -13,7 +13,6 @@
  */
 
 ?>
-<<<<<<< Updated upstream
 			<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -22,38 +21,6 @@
 <!-- Footer -->
 	<section id="footer">
 		<div class="container">
-			<div class="row text-center text-xs-center text-sm-left text-md-left">
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<h5>Quick links</h5>
-					<ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
-					</ul>
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<h5>Quick links</h5>
-					<ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
-					</ul>
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<h5>Quick links</h5>
-					<ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-						<li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
-					</ul>
-				</div>
-			</div>
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
 					<ul class="list-unstyled list-inline social text-center">
@@ -81,53 +48,3 @@
 
 	</body>
 </html>
-=======
-<footer id="site-footer" role="contentinfo" class="header-footer-group">
-
-    <div class="section-inner">
-
-        <div class="footer-credits">
-
-            <p class="footer-copyright">&copy;
-                <?php
-				echo date_i18n(
-					/* translators: Copyright date format, see https://www.php.net/manual/datetime.format.php */
-					_x('Y', 'copyright date format', 'twentytwenty')
-				);
-				?>
-                <a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-            </p><!-- .footer-copyright -->
-
-            <p class="powered-by-wordpress">
-                <a href="<?php echo esc_url(__('https://wordpress.org/', 'twentytwenty')); ?>">
-                    <?php _e('Powered by WordPress', 'twentytwenty'); ?>
-                </a>
-            </p><!-- .powered-by-wordpress -->
-
-        </div><!-- .footer-credits -->
-
-        <a class="to-the-top" href="#site-header">
-            <span class="to-the-top-long">
-                <?php
-				/* translators: %s: HTML character for up arrow. */
-				printf(__('To the top %s', 'twentytwenty'), '<span class="arrow" aria-hidden="true">&uarr;</span>');
-				?>
-            </span><!-- .to-the-top-long -->
-            <span class="to-the-top-short">
-                <?php
-				/* translators: %s: HTML character for up arrow. */
-				printf(__('Up %s', 'twentytwenty'), '<span class="arrow" aria-hidden="true">&uarr;</span>');
-				?>
-            </span><!-- .to-the-top-short -->
-        </a><!-- .to-the-top -->
-
-    </div><!-- .section-inner -->
-
-</footer><!-- #site-footer -->
-
-<?php wp_footer(); ?>
-
-</body>
-
-</html>
->>>>>>> Stashed changes
