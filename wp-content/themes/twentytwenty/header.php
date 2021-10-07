1b71c15fca281bc7dc2b652186423c8293658e2f
nguyenthengocit37 <nguyenthengocit37@gmail.com>
2021-10-07 11:44:41 +0700

    commit wordpess

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -20,7 +20,7 @@
 
 		<link rel="profile" href="https://gmpg.org/xfn/11">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 		<?php wp_head(); ?>
 
 	</head>
