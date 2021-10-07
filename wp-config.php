ce14e1d22084a3442fa65e380d4db5b91dfd62fd
Longkunz <cuvip1325@gmail.com>
2021-10-07 08:44:30 +0700

    Change database & site url

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wordpress_581' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'mysql' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
