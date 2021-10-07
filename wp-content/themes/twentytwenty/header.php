ef9d631dbf6aefd54aa89accc2f0d5ff462c46f3
Võ Văn Trường <56033678+vovantruong@users.noreply.github.com>
2021-10-07 09:45:11 +0700

    update css footer

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -20,6 +20,10 @@
 
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<?php wp_head(); ?>
 
 	</head>
