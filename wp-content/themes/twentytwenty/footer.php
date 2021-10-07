c7a1a7297240d2057e9b2b302290814730c64e1a
VoDangVinh <01686228222vinh@gmail.com>
2021-10-07 09:11:59 +0700

    update footer

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -12,48 +13,78 @@
  */
 
 ?>
-			<footer id="site-footer" role="contentinfo" class="header-footer-group">
-				<div class="section-inner">
-					<div class="footer-credits">
+<footer class="bg-primary text-white text-center text-lg-start">
+	<!-- Grid container -->
+	<div class="container p-4">
+		<!--Grid row-->
+		<div class="row">
+			<!--Grid column-->
+			<div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+				<h5 class="text-uppercase">Footer Content</h5>
 
-						<p class="footer-copyright">&copy;
-							<?php
-							echo date_i18n(
-								/* translators: Copyright date format, see https://www.php.net/manual/datetime.format.php */
-								_x( 'Y', 'copyright date format', 'twentytwenty' )
-							);
-							?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						</p><!-- .footer-copyright -->
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+					molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+					voluptatem veniam, est atque cumque eum delectus sint!
+				</p>
+			</div>
+			<!--Grid column-->
 
-						<p class="powered-by-wordpress">
-							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress', 'twentytwenty' ); ?>
-							</a>
-						</p><!-- .powered-by-wordpress -->
+			<!--Grid column-->
+			<div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+				<h5 class="text-uppercase">Links</h5>
 
-					</div><!-- .footer-credits -->
+				<ul class="list-unstyled mb-0">
+					<li>
+						<a href="#!" class="text-white">Link 1</a>
+					</li>
+					<li>
+						<a href="#!" class="text-white">Link 2</a>
+					</li>
+					<li>
+						<a href="#!" class="text-white">Link 3</a>
+					</li>
+					<li>
+						<a href="#!" class="text-white">Link 4</a>
+					</li>
+				</ul>
+			</div>
+			<!--Grid column-->
 
-					<a class="to-the-top" href="#site-header">
-						<span class="to-the-top-long">
-							<?php
-							/* translators: %s: HTML character for up arrow. */
-							printf( __( 'To the top %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
-							?>
-						</span><!-- .to-the-top-long -->
-						<span class="to-the-top-short">
-							<?php
-							/* translators: %s: HTML character for up arrow. */
-							printf( __( 'Up %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
-							?>
-						</span><!-- .to-the-top-short -->
-					</a><!-- .to-the-top -->
+			<!--Grid column-->
+			<div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+				<h5 class="text-uppercase mb-0">Links</h5>
 
-				</div><!-- .section-inner -->
+				<ul class="list-unstyled">
+					<li>
+						<a href="#!" class="text-white">Link 1</a>
+					</li>
+					<li>
+						<a href="#!" class="text-white">Link 2</a>
+					</li>
+					<li>
+						<a href="#!" class="text-white">Link 3</a>
+					</li>
+					<li>
+						<a href="#!" class="text-white">Link 4</a>
+					</li>
+				</ul>
+			</div>
+			<!--Grid column-->
+		</div>
+		<!--Grid row-->
+	</div>
+	<!-- Grid container -->
 
-			</footer><!-- #site-footer -->
+	<!-- Copyright -->
+	<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+		© 2020 Copyright:
+		<a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+	</div>
+	<!-- Copyright -->
 
-		<?php wp_footer(); ?>
+	<?php wp_footer(); ?>
 
 	</body>
-</html>
+
+	</html>
--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -17,7 +17,9 @@
 
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
-
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+		rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+		crossorigin="anonymous">
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 
 		<?php wp_head(); ?>
