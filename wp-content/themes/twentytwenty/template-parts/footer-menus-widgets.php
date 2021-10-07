f2524c330ecba21720e5a25aeeb899025449b719
mytien0906 <thaitiendrm@gmail.com>
2021-10-08 03:07:21 +0700

    update custom footer

--- wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
+++ wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Displays the menus and widgets at the end of the main element.
  * Visually, this output is presented as part of the footer element.
@@ -8,15 +9,15 @@
  * @since Twenty Twenty 1.0
  */
 
-$has_footer_menu = has_nav_menu( 'footer' );
-$has_social_menu = has_nav_menu( 'social' );
+$has_footer_menu = has_nav_menu('footer');
+$has_social_menu = has_nav_menu('social');
 
-$has_sidebar_1 = is_active_sidebar( 'sidebar-1' );
-$has_sidebar_2 = is_active_sidebar( 'sidebar-2' );
+$has_sidebar_1 = is_active_sidebar('sidebar-1');
+$has_sidebar_2 = is_active_sidebar('sidebar-2');
 
 // Only output the container if there are elements to display.
-if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) {
-	?>
+if ($has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2) {
+?>
 
 	<div class="footer-nav-widgets-wrapper header-footer-group">
 
@@ -29,12 +30,13 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) 
 			$footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
 			$footer_top_classes .= $has_social_menu ? ' has-social-menu' : '';
 
-			if ( $has_footer_menu || $has_social_menu ) {
-				?>
-				<div class="footer-top<?php echo $footer_top_classes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?>">
-					<?php if ( $has_footer_menu ) { ?>
+			if ($has_footer_menu || $has_social_menu) {
+			?>
+				<div class="footer-top<?php echo $footer_top_classes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output 
+										?>">
+					<?php if ($has_footer_menu) { ?>
 
-						<nav aria-label="<?php esc_attr_e( 'Footer', 'twentytwenty' ); ?>" role="navigation" class="footer-menu-wrapper">
+						<nav aria-label="<?php esc_attr_e('Footer', 'twentytwenty'); ?>" role="navigation" class="footer-menu-wrapper">
 
 							<ul class="footer-menu reset-list-style">
 								<?php
@@ -52,9 +54,9 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) 
 						</nav><!-- .site-nav -->
 
 					<?php } ?>
-					<?php if ( $has_social_menu ) { ?>
+					<?php if ($has_social_menu) { ?>
 
-						<nav aria-label="<?php esc_attr_e( 'Social links', 'twentytwenty' ); ?>" class="footer-social-wrapper">
+						<nav aria-label="<?php esc_attr_e('Social links', 'twentytwenty'); ?>" class="footer-social-wrapper">
 
 							<ul class="social-menu footer-social reset-list-style social-icons fill-children-current-color">
 
@@ -83,34 +85,29 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) 
 				</div><!-- .footer-top -->
 
 			<?php } ?>
-
-			<?php if ( $has_sidebar_1 || $has_sidebar_2 ) { ?>
-
-				<aside class="footer-widgets-outer-wrapper" role="complementary">
-
-					<div class="footer-widgets-wrapper">
-
-						<?php if ( $has_sidebar_1 ) { ?>
-
-							<div class="footer-widgets column-one grid-item">
-								<?php dynamic_sidebar( 'sidebar-1' ); ?>
+			<div class="row text-center text-xs-center text-sm-left text-md-left">
+				<div class="col-xs-12 col-sm-8 col-md-8">
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="d-block d-md-flex footer-widgets justify-content-around">
+								<?php dynamic_sidebar('sidebar-1'); ?>
 							</div>
-
-						<?php } ?>
-
-						<?php if ( $has_sidebar_2 ) { ?>
-
-							<div class="footer-widgets column-two grid-item">
-								<?php dynamic_sidebar( 'sidebar-2' ); ?>
-							</div>
-
-						<?php } ?>
-
-					</div><!-- .footer-widgets-wrapper -->
-
-				</aside><!-- .footer-widgets-outer-wrapper -->
-
-			<?php } ?>
+						</div>
+					</div>
+				</div>
+				<!-- <div class="col-xs-12 col-sm-4 col-md-4">
+					<div class="footer-widgets column-one grid-item">
+						<?php dynamic_sidebar('sidebar-1'); ?>
+					</div>
+				</div> -->
+				<div class="col-xs-12 col-sm-4 col-md-4">
+				<div class="footer-widgets column-one grid-item">
+						<?php dynamic_sidebar('sidebar-2'); ?>
+					</div>
+				</div>
+				
+			</div>
+			
 
 		</div><!-- .footer-inner -->
 
