a6b404126d35a8b03090f7cfcb17569f397cc86a
thanhquan1306 <19211tt0882@mail.tdc.edu.vn>
2021-10-07 09:21:13 +0700

    update footer

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -179,6 +179,12 @@
 				get_template_part( 'template-parts/modal-search' );
 			}
 			?>
+            <!-- Bootstrap -->
+            <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+            <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <!-- Font Awesome -->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 		</header><!-- #site-header -->
 
