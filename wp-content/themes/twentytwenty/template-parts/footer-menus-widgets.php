33c995e16bd905520756599627347a8b4d5a4f9b
Trung Nguyen Huynh Quoc <19211tt0744@mail.tdc.edu.vn>
2021-10-08 09:33:24 +0700

    Add sidebar-3

--- wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
+++ wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <?php
 /**
  * Displays the menus and widgets at the end of the main element.
@@ -13,107 +14,32 @@ $has_social_menu = has_nav_menu( 'social' );
 
 $has_sidebar_1 = is_active_sidebar( 'sidebar-1' );
 $has_sidebar_2 = is_active_sidebar( 'sidebar-2' );
+$has_sidebar_3 = is_active_sidebar( 'sidebar-3' );
 
 // Only output the container if there are elements to display.
 if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) {
 	?>
 
-	<div class="footer-nav-widgets-wrapper header-footer-group">
-
-		<div class="footer-inner section-inner">
-
-			<?php
-
-			$footer_top_classes = '';
-
-			$footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
-			$footer_top_classes .= $has_social_menu ? ' has-social-menu' : '';
-
-			if ( $has_footer_menu || $has_social_menu ) {
-				?>
-				<div class="footer-top<?php echo $footer_top_classes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?>">
-					<?php if ( $has_footer_menu ) { ?>
-
-						<nav aria-label="<?php esc_attr_e( 'Footer', 'twentytwenty' ); ?>" role="navigation" class="footer-menu-wrapper">
-
-							<ul class="footer-menu reset-list-style">
-								<?php
-								wp_nav_menu(
-									array(
-										'container'      => '',
-										'depth'          => 1,
-										'items_wrap'     => '%3$s',
-										'theme_location' => 'footer',
-									)
-								);
-								?>
-							</ul>
-
-						</nav><!-- .site-nav -->
-
-					<?php } ?>
-					<?php if ( $has_social_menu ) { ?>
-
-						<nav aria-label="<?php esc_attr_e( 'Social links', 'twentytwenty' ); ?>" class="footer-social-wrapper">
-
-							<ul class="social-menu footer-social reset-list-style social-icons fill-children-current-color">
-
-								<?php
-								wp_nav_menu(
-									array(
-										'theme_location'  => 'social',
-										'container'       => '',
-										'container_class' => '',
-										'items_wrap'      => '%3$s',
-										'menu_id'         => '',
-										'menu_class'      => '',
-										'depth'           => 1,
-										'link_before'     => '<span class="screen-reader-text">',
-										'link_after'      => '</span>',
-										'fallback_cb'     => '',
-									)
-								);
-								?>
-
-							</ul><!-- .footer-social -->
-
-						</nav><!-- .footer-social-wrapper -->
-
-					<?php } ?>
-				</div><!-- .footer-top -->
-
-			<?php } ?>
-
-			<?php if ( $has_sidebar_1 || $has_sidebar_2 ) { ?>
-
-				<aside class="footer-widgets-outer-wrapper" role="complementary">
-
-					<div class="footer-widgets-wrapper">
-
-						<?php if ( $has_sidebar_1 ) { ?>
-
-							<div class="footer-widgets column-one grid-item">
-								<?php dynamic_sidebar( 'sidebar-1' ); ?>
-							</div>
-
-						<?php } ?>
-
-						<?php if ( $has_sidebar_2 ) { ?>
-
-							<div class="footer-widgets column-two grid-item">
-								<?php dynamic_sidebar( 'sidebar-2' ); ?>
-							</div>
-
-						<?php } ?>
-
-					</div><!-- .footer-widgets-wrapper -->
-
-				</aside><!-- .footer-widgets-outer-wrapper -->
-
-			<?php } ?>
-
-		</div><!-- .footer-inner -->
-
-	</div><!-- .footer-nav-widgets-wrapper -->
+<section id="footer" style="padding-bottom: 0;">
+		<div class="container">
+			<div class="row text-center text-xs-center text-sm-left text-md-left">
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<ul class="list-unstyled quick-links">
+						<?php dynamic_sidebar('sidebar-1'); ?>
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<ul class="list-unstyled quick-links">
+						<?php dynamic_sidebar('sidebar-2'); ?>
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<ul class="list-unstyled quick-links">
+						<?php dynamic_sidebar('sidebar-3'); ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
 
 <?php } ?>
