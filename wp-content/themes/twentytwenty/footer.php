95ef11126f5a630529b650616195e1be071742c6
Van Nghi <19211tt1981@mail.tdc.edu.vn>
2021-10-08 11:46:23 +0700

    Update file

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -3,10 +3,62 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
-
+<?php
+$has_sidebar_1 = is_active_sidebar( 'sidebar-1' );
+$has_sidebar_2 = is_active_sidebar( 'sidebar-2' );
+$has_sidebar_3 = is_active_sidebar( 'sidebar-3' );
+$has_sidebar_4 = is_active_sidebar( 'sidebar-4' );
+?>
 <!-- Footer -->
 	<section id="footer">
 		<div class="container">
+            <div class="row text-center text-xs-center text-sm-left text-md-left">
+                <?php if ( $has_sidebar_1 || $has_sidebar_2 ) { ?>
+                <aside class="footer-widgets-outer-wrapper" role="complementary">
+
+                    <div class="footer-widgets-wrapper">
+
+                        <?php if ( $has_sidebar_1 ) { ?>
+
+                            <div class="footer-widgets column-one grid-item">
+                                <?php dynamic_sidebar( 'sidebar-1' ); ?>
+                            </div>
+
+                        <?php } ?>
+                </aside><!-- .footer-widgets-outer-wrapper -->
+
+                <?php } ?>
+                <div class="col-xs-12 col-sm-4 col-md-4">
+                    <h5>Quick links</h5>
+                    <ul class="list-unstyled quick-links">
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4">
+                    <h5>Quick links</h5>
+                    <ul class="list-unstyled quick-links">
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4">
+                    <h5>Quick links</h5>
+                    <ul class="list-unstyled quick-links">
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+                        <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+                        <li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
+                    </ul>
+                </div>
+            </div>
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
 					<ul class="list-unstyled list-inline social text-center">
--- wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
+++ wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
@@ -13,106 +13,127 @@ $has_social_menu = has_nav_menu( 'social' );
 
 $has_sidebar_1 = is_active_sidebar( 'sidebar-1' );
 $has_sidebar_2 = is_active_sidebar( 'sidebar-2' );
+$has_sidebar_3 = is_active_sidebar( 'sidebar-3' );
+$has_sidebar_4 = is_active_sidebar( 'sidebar-4' );
 
 // Only output the container if there are elements to display.
 if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) {
 	?>
 
 	<div class="footer-nav-widgets-wrapper header-footer-group">
+        <section id="footer">
+            <div class="container">
+                <div class="row text-center text-xs-center text-sm-left text-md-left">
+                        <div class="footer-inner section-inner">
 
-		<div class="footer-inner section-inner">
+                            <?php
 
-			<?php
+                            $footer_top_classes = '';
 
-			$footer_top_classes = '';
+                            $footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
+                            $footer_top_classes .= $has_social_menu ? ' has-social-menu' : '';
 
-			$footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
-			$footer_top_classes .= $has_social_menu ? ' has-social-menu' : '';
+                            if ( $has_footer_menu || $has_social_menu ) {
+                                ?>
+                                <div class="footer-top<?php echo $footer_top_classes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?>">
+                                    <?php if ( $has_footer_menu ) { ?>
 
-			if ( $has_footer_menu || $has_social_menu ) {
-				?>
-				<div class="footer-top<?php echo $footer_top_classes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?>">
-					<?php if ( $has_footer_menu ) { ?>
+                                        <nav aria-label="<?php esc_attr_e( 'Footer', 'twentytwenty' ); ?>" role="navigation" class="footer-menu-wrapper">
 
-						<nav aria-label="<?php esc_attr_e( 'Footer', 'twentytwenty' ); ?>" role="navigation" class="footer-menu-wrapper">
+                                            <ul class="footer-menu reset-list-style">
+                                                <?php
+                                                wp_nav_menu(
+                                                    array(
+                                                        'container'      => '',
+                                                        'depth'          => 1,
+                                                        'items_wrap'     => '%3$s',
+                                                        'theme_location' => 'footer',
+                                                    )
+                                                );
+                                                ?>
+                                            </ul>
 
-							<ul class="footer-menu reset-list-style">
-								<?php
-								wp_nav_menu(
-									array(
-										'container'      => '',
-										'depth'          => 1,
-										'items_wrap'     => '%3$s',
-										'theme_location' => 'footer',
-									)
-								);
-								?>
-							</ul>
+                                        </nav><!-- .site-nav -->
 
-						</nav><!-- .site-nav -->
+                                    <?php } ?>
+                                    <?php if ( $has_social_menu ) { ?>
 
-					<?php } ?>
-					<?php if ( $has_social_menu ) { ?>
+                                        <nav aria-label="<?php esc_attr_e( 'Social links', 'twentytwenty' ); ?>" class="footer-social-wrapper">
 
-						<nav aria-label="<?php esc_attr_e( 'Social links', 'twentytwenty' ); ?>" class="footer-social-wrapper">
+                                            <ul class="social-menu footer-social reset-list-style social-icons fill-children-current-color">
 
-							<ul class="social-menu footer-social reset-list-style social-icons fill-children-current-color">
+                                                <?php
+                                                wp_nav_menu(
+                                                    array(
+                                                        'theme_location'  => 'social',
+                                                        'container'       => '',
+                                                        'container_class' => '',
+                                                        'items_wrap'      => '%3$s',
+                                                        'menu_id'         => '',
+                                                        'menu_class'      => '',
+                                                        'depth'           => 1,
+                                                        'link_before'     => '<span class="screen-reader-text">',
+                                                        'link_after'      => '</span>',
+                                                        'fallback_cb'     => '',
+                                                    )
+                                                );
+                                                ?>
 
-								<?php
-								wp_nav_menu(
-									array(
-										'theme_location'  => 'social',
-										'container'       => '',
-										'container_class' => '',
-										'items_wrap'      => '%3$s',
-										'menu_id'         => '',
-										'menu_class'      => '',
-										'depth'           => 1,
-										'link_before'     => '<span class="screen-reader-text">',
-										'link_after'      => '</span>',
-										'fallback_cb'     => '',
-									)
-								);
-								?>
+                                            </ul><!-- .footer-social -->
 
-							</ul><!-- .footer-social -->
+                                        </nav><!-- .footer-social-wrapper -->
 
-						</nav><!-- .footer-social-wrapper -->
+                                    <?php } ?>
+                                </div><!-- .footer-top -->
 
-					<?php } ?>
-				</div><!-- .footer-top -->
+                            <?php } ?>
 
-			<?php } ?>
+                            <?php if ( $has_sidebar_1 || $has_sidebar_2 ) { ?>
 
-			<?php if ( $has_sidebar_1 || $has_sidebar_2 ) { ?>
+                                <aside class="footer-widgets-outer-wrapper" role="complementary">
 
-				<aside class="footer-widgets-outer-wrapper" role="complementary">
+                                    <div class="footer-widgets-wrapper">
 
-					<div class="footer-widgets-wrapper">
+                                        <?php if ( $has_sidebar_1 ) { ?>
 
-						<?php if ( $has_sidebar_1 ) { ?>
+                                            <div class="footer-widgets column-one grid-item">
+                                                <?php dynamic_sidebar( 'sidebar-1' ); ?>
+                                            </div>
 
-							<div class="footer-widgets column-one grid-item">
-								<?php dynamic_sidebar( 'sidebar-1' ); ?>
-							</div>
+                                        <?php } ?>
 
-						<?php } ?>
+                                        <?php if ( $has_sidebar_2 ) { ?>
 
-						<?php if ( $has_sidebar_2 ) { ?>
+                                            <div class="footer-widgets column-two grid-item">
+                                                <?php dynamic_sidebar( 'sidebar-2' ); ?>
+                                            </div>
 
-							<div class="footer-widgets column-two grid-item">
-								<?php dynamic_sidebar( 'sidebar-2' ); ?>
-							</div>
+                                        <?php } ?>
+                                        <?php if ( $has_sidebar_3 ) { ?>
 
-						<?php } ?>
+                                            <div class="footer-widgets column-three grid-item">
+                                                <?php dynamic_sidebar( 'sidebar-3' ); ?>
+                                            </div>
 
-					</div><!-- .footer-widgets-wrapper -->
+                                        <?php } ?>
+                                        <?php if ( $has_sidebar_4 ) { ?>
 
-				</aside><!-- .footer-widgets-outer-wrapper -->
+                                            <div class="footer-widgets column-four grid-item">
+                                                <?php dynamic_sidebar( 'sidebar-3' ); ?>
+                                            </div>
 
-			<?php } ?>
+                                        <?php } ?>
 
-		</div><!-- .footer-inner -->
+                                    </div><!-- .footer-widgets-wrapper -->
+
+                                </aside><!-- .footer-widgets-outer-wrapper -->
+
+                            <?php } ?>
+
+                        </div><!-- .footer-inner -->
+            </div>
+        </section>
+
 
 	</div><!-- .footer-nav-widgets-wrapper -->
 
