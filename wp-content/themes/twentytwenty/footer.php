37c85c49fdb674868bf3f0d983efc71b37f4c5d1
quocviet0706 <nguyenquocviet0706@gmail.com>
2021-10-09 11:20:43 +0700

    update footer 09/10/21

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -19,59 +19,68 @@
 <!-- Footer -->
 <style>
     body {
-        font-family: 'Adamina';font-size:16px;
+        font-family: 'Adamina';
+        font-size: 16px;
     }
 </style>
 <section id="footer">
     <div class="container">
         <div class="row text-center text-xs-center text-sm-left text-md-left">
             <div class="col-xs-12 col-sm-4 col-md-4">
-                <h5>Quick links</h5>
+
+                <h5 class="wp-block-group__inner-container">Recent Posts</h5>
                 <ul class="list-unstyled quick-links">
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+                    <li><a href="http://wordpress.local/2021/09/22/solskjaer-ronaldo-khong-chi-dao-cau-thu-man-utd/">Solskjaer:
+                            ‘Ronaldo không chỉ đạo cầu thủ Man Utd’</a></li>
+                    <li><a href="http://wordpress.local/2021/09/22/test1/">Man Utd bị loại khỏi Cup Liên đoàn</a></li>
+                    <li><a href="http://wordpress.local/2021/09/16/hello-world/">Việt Nam thua sát Nga ở vòng 1/8 futsal
+                            World Cup</a></li>
                 </ul>
             </div>
             <div class="col-xs-12 col-sm-4 col-md-4">
-                <h5>Quick links</h5>
+                <h5>Recent Comments</h5>
                 <ul class="list-unstyled quick-links">
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+                    <li class="wp-block-latest-comments__comment">
+                        <a class="wp-block-latest-comments__comment-author"
+                           href="https://wordpress.org/">A WordPress Commenter</a>
+                        <a class="wp-block-latest-comments__comment-link"
+                           href="http://wordpress.local/2021/09/16/hello-world/#comment-1">Việt Nam
+                            thua sát Nga ở vòng 1/8 futsal World Cup</a>
+                    </li>
                 </ul>
             </div>
             <div class="col-xs-12 col-sm-4 col-md-4">
-                <h5>Quick links</h5>
+                <h5>Archives</h5>
                 <ul class="list-unstyled quick-links">
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-                    <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-                    <li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
+                    <li><a href="http://wordpress.local/2021/09/">September 2021</a></li>
                 </ul>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
                 <ul class="list-unstyled list-inline social text-center">
-                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-google-plus"></i></a></li>
-                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i
+                                    class="fa fa-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i
+                                    class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i
+                                    class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i
+                                    class="fa fa-google-plus"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i
+                                    class="fa fa-envelope"></i></a></li>
                 </ul>
             </div>
             <hr>
         </div>
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-                <p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
-                <p class="h6" style="font-family: 'Adamina';font-size:16px;">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
+                <p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a
+                    Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis,
+                    MN]</p>
+                <p class="h6" style="font-family: 'Adamina';font-size:16px;">© All right Reversed.<a
+                            class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a>
+                </p>
             </div>
             <hr>
         </div>
@@ -79,7 +88,7 @@
 </section>
 <!-- ./Footer -->
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
 
 </html>
