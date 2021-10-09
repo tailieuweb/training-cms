7333e345ccbac06197eacb00682ed39e00946ee6
thinhnguyen-ken <nguyenvanthinh081199@gmail.com>
2021-10-09 11:04:06 +0700

    update footer advanced ver 1.0 09/10/2021

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -2,7 +2,7 @@
 
 <section id="footer">
     <div class="container">
-
+        
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
                 <ul class="list-unstyled list-inline social text-center">
