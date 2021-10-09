2a7678469cced336301186e91fb6eea034a0aacc
khanhu26 <80993489+khanhu26@users.noreply.github.com>
2021-10-09 10:38:09 +0700

    Update widgets

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -14,7 +14,6 @@
 <html class="no-js" <?php language_attributes(); ?>>
 
 	<head>
-
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
