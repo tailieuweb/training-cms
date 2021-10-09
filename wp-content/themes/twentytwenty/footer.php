b38e19a506a5de69fd76130a3fa167e2b59bd790
sangtm97 <minhsangtrieu94@gmail.com>
2021-10-09 11:05:47 +0700

    update

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -23,7 +23,6 @@
 <!-- Footer -->
 <section id="footer">
     <div class="container">
-    <div class="row text-center text-xs-center text-sm-left text-md-left">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
 				<ul class="list-unstyled list-inline social text-center">
