ab75921649aa0dd26a2b367d6e87183876a47304
hungnguyen7724 <hungnguyen7724@gmail.com>
2021-10-09 11:45:31 +0700

    update-footer-nangcao

--- wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
+++ wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Displays the menus and widgets at the end of the main element.
  * Visually, this output is presented as part of the footer element.
@@ -84,27 +85,35 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) 
 
 			<?php } ?>
 
-			<?php if ( $has_sidebar_1 || $has_sidebar_2 ) { ?>
+			<?php if ($has_sidebar_1 || $has_sidebar_2 || $has_sidebar_3 ) { ?>
 
 				<aside class="footer-widgets-outer-wrapper" role="complementary">
 
 					<div class="footer-widgets-wrapper">
 
-						<?php if ( $has_sidebar_1 ) { ?>
+						<!-- <?php if ( $has_sidebar_1 ) { ?>
 
 							<div class="footer-widgets column-one grid-item">
-								<?php dynamic_sidebar( 'sidebar-1' ); ?>
+								<?php dynamic_sidebar('sidebar-1'); ?>
 							</div>
 
 						<?php } ?>
 
-						<?php if ( $has_sidebar_2 ) { ?>
+						<?php if ($has_sidebar_2) { ?>
 
 							<div class="footer-widgets column-two grid-item">
-								<?php dynamic_sidebar( 'sidebar-2' ); ?>
+								<?php dynamic_sidebar('sidebar-2'); ?>
 							</div>
 
 						<?php } ?>
+
+						<?php if ($has_sidebar_3) { ?>
+
+							<div class="footer-widgets column-three grid-item">
+								<?php dynamic_sidebar('sidebar-3'); ?>
+							</div>
+
+						<?php } ?>  -->
 
 					</div><!-- .footer-widgets-wrapper -->
 
