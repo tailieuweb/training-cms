9ef4e814ace3141bbe0578d3c6b3bf3db2170f71
VoDangVinh2000 <01686228222vinh@gmail.com>
2021-10-13 20:14:10 +0700

    update

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -24,8 +24,6 @@
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 		
 		<link rel="stylesheet" href="style.css">
-		<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<?php wp_head(); ?>
