6c0bd0e079dced75331285ca7475c012244d5109
VoDangVinh2000 <01686228222vinh@gmail.com>
2021-10-14 07:32:20 +0700

    update conflict footer

--- wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
+++ wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
@@ -13,22 +13,29 @@ $has_social_menu = has_nav_menu( 'social' );
 
 $has_sidebar_1 = is_active_sidebar( 'sidebar-1' );
 $has_sidebar_2 = is_active_sidebar( 'sidebar-2' );
-$has_sidebar_2 = is_active_sidebar( 'sidebar-3' );
 
 // Only output the container if there are elements to display.
-if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 || $has_sidebar_3 ) {
+if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) {
 	?>
+
 	<div class="footer-nav-widgets-wrapper header-footer-group">
+
 		<div class="footer-inner section-inner">
+
 			<?php
+
 			$footer_top_classes = '';
+
 			$footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
 			$footer_top_classes .= $has_social_menu ? ' has-social-menu' : '';
+
 			if ( $has_footer_menu || $has_social_menu ) {
 				?>
 				<div class="footer-top<?php echo $footer_top_classes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?>">
 					<?php if ( $has_footer_menu ) { ?>
+
 						<nav aria-label="<?php esc_attr_e( 'Footer', 'twentytwenty' ); ?>" role="navigation" class="footer-menu-wrapper">
+
 							<ul class="footer-menu reset-list-style">
 								<?php
 								wp_nav_menu(
@@ -41,11 +48,16 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ||
 								);
 								?>
 							</ul>
+
 						</nav><!-- .site-nav -->
+
 					<?php } ?>
 					<?php if ( $has_social_menu ) { ?>
+
 						<nav aria-label="<?php esc_attr_e( 'Social links', 'twentytwenty' ); ?>" class="footer-social-wrapper">
+
 							<ul class="social-menu footer-social reset-list-style social-icons fill-children-current-color">
+
 								<?php
 								wp_nav_menu(
 									array(
@@ -62,39 +74,43 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ||
 									)
 								);
 								?>
+
 							</ul><!-- .footer-social -->
+
 						</nav><!-- .footer-social-wrapper -->
+
 					<?php } ?>
 				</div><!-- .footer-top -->
+
 			<?php } ?>
-			<?php if ( $has_sidebar_1 || $has_sidebar_2 || $has_sidebar_3) { ?>
+
+			<?php if ( $has_sidebar_1 || $has_sidebar_2 ) { ?>
+
 				<aside class="footer-widgets-outer-wrapper" role="complementary">
+
 					<div class="footer-widgets-wrapper">
+
 						<?php if ( $has_sidebar_1 ) { ?>
+
 							<div class="footer-widgets column-one grid-item">
 								<?php dynamic_sidebar( 'sidebar-1' ); ?>
 							</div>
+
 						<?php } ?>
+
 						<?php if ( $has_sidebar_2 ) { ?>
+
 							<div class="footer-widgets column-two grid-item">
 								<?php dynamic_sidebar( 'sidebar-2' ); ?>
 							</div>
+
 						<?php } ?>
-						<?php if ( $has_sidebar_3 ) { ?>
-						<div class="footer-widgets column-three grid-item">
-								<?php dynamic_sidebar( 'sidebar-3' ); ?>
-								</div>
-						<?php } ?>
-						<?php if ( $has_sidebar_3 ) { ?>
-							<div class="footer-widgets column-three grid-item">
-								<?php dynamic_sidebar( 'sidebar-3' ); ?>
-							</div>
-						<?php } ?>
+
 					</div><!-- .footer-widgets-wrapper -->
 
-				<!-- aside>.footer-widgets-outer-wrapper -->
+				</aside><!-- .footer-widgets-outer-wrapper -->
 
-			<!-- php } ?> -->
+			<?php } ?>
 
 		</div><!-- .footer-inner -->
 
