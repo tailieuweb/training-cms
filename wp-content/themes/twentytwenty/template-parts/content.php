94e24b18c72b027cb6ca603ae6e7f8496279310e
TranNgocTam <ngoctam2303001@gmail.com>
2021-10-22 08:56:21 +0700

    Update modum detail (6)

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -12,83 +12,152 @@
  */
 
 ?>
+<?php if( is_single()) { ?>
+<div class="row">
+    <div class="col-md-3">
 
+    </div>
+    <div class="col-md-6">
+        <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+            <?php
+
+get_template_part('template-parts/entry-header');
+
+if (! is_search()) {
+get_template_part('template-parts/featured-image');
+}
+
+?>
+
+            <div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
+                <!-- overviewline -->
+                <div class="overviewline"></div>
+                <div class="entry-content-1">
+
+                    <?php
+if (is_search() || ! is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
+the_excerpt();
+} else {
+the_content(__('Continue reading', 'twentytwenty'));
+}
+?>
+
+                </div><!-- .entry-content -->
+
+            </div><!-- .post-inner -->
+
+            <div class="section-inner-1">
+                <?php
+					wp_link_pages(
+					array(
+					'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
+					'after'       => '</nav>',
+					'link_before' => '<span class="page-number">',
+					'link_after'  => '</span>',
+					)
+					);
+					
+					edit_post_link(); // edit last
+					
+					
+					// Single bottom post meta.
+					twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
+
+					if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
+					get_template_part('template-parts/entry-author-bio');
+					}
+				?>
+
+            </div><!-- .section-inner -->
+
+            <?php
+
+if (is_single()) {
+get_template_part('template-parts/navigation');
+}
+
+/*
+* Output comments wrapper if it's a post, or if comments are open,
+* or if there's a comment number – and check for password.
+*/
+
+?>
+
+        </article><!-- .post -->
+    </div>
+    <div class="col-md-3">
+
+    </div>
+</div>
+<?php } ?>
+
+<!-- home -->
+<?php if( !is_single()) { ?>
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<?php
+    <?php
 
-	get_template_part( 'template-parts/entry-header' );
+get_template_part('template-parts/entry-header');
 
-	if ( ! is_search() ) {
-		get_template_part( 'template-parts/featured-image' );
-	}
+if (! is_search()) {
+get_template_part('template-parts/featured-image');
+}
 
-	?>
+?>
 
-	<div class="post-inner <?php echo is_page_template( 'templates/template-full-width.php' ) ? '' : 'thin'; ?> ">
+    <div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
 
-		<div class="entry-content">
+        <div class="entry-content">
 
-			<?php
-			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
-				the_excerpt();
-			} else {
-				the_content( __( 'Continue reading', 'twentytwenty' ) );
-			}
-			?>
+            <?php
+if (is_search() || ! is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
+the_excerpt();
+} else {
 
-		</div><!-- .entry-content -->
+the_content(__('Continue reading', 'twentytwenty'));
+}
+?>
 
-	</div><!-- .post-inner -->
+        </div><!-- .entry-content -->
 
-	<div class="section-inner">
-		<?php
-		wp_link_pages(
-			array(
-				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
-				'after'       => '</nav>',
-				'link_before' => '<span class="page-number">',
-				'link_after'  => '</span>',
-			)
-		);
+    </div><!-- .post-inner -->
 
-		edit_post_link();
+    <div class="section-inner">
+        <?php
+wp_link_pages(
+array(
+'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
+'after'       => '</nav>',
+'link_before' => '<span class="page-number">',
+'link_after'  => '</span>',
+)
+);
 
-		// Single bottom post meta.
-		twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
+edit_post_link();
 
-		if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
+// Single bottom post meta.
+twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
 
-			get_template_part( 'template-parts/entry-author-bio' );
+if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
+get_template_part('template-parts/entry-author-bio');
+}
+?>
 
-		}
-		?>
+    </div><!-- .section-inner -->
 
-	</div><!-- .section-inner -->
+    <?php
 
-	<?php
+if (is_single()) {
+get_template_part('template-parts/navigation');
+}
 
-	if ( is_single() ) {
+/*
+* Output comments wrapper if it's a post, or if comments are open,
+* or if there's a comment number – and check for password.
+*/
 
-		get_template_part( 'template-parts/navigation' );
-
-	}
-
-	/*
-	 * Output comments wrapper if it's a post, or if comments are open,
-	 * or if there's a comment number – and check for password.
-	 */
-	if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
-		?>
-
-		<div class="comments-wrapper section-inner">
-
-			<?php comments_template(); ?>
-
-		</div><!-- .comments-wrapper -->
-
-		<?php
-	}
-	?>
+?>
 
 </article><!-- .post -->
+<?php } ?>
