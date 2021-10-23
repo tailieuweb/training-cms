b4ca55f8de709b36bdb607ba6150d153726fbc6e
quocviet0706 <nguyenquocviet0706@gmail.com>
2021-10-23 13:49:44 +0700

    update footer

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -1,80 +1,78 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<link href='https://fonts.googleapis.com/css?family=Adamina' rel='stylesheet'>
-<script src="https://kit.fontawesome.com/473ccb2a41.js" crossorigin="anonymous"></script>
-<style>
-    body {
-        font-family: 'Adamina';
-        font-size: 16px;
-    }
-</style>
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+$has_sidebar_1 = is_active_sidebar('sidebar-1');
+$has_sidebar_2 = is_active_sidebar('sidebar-2');
+$has_sidebar_3 = is_active_sidebar('sidebar-3');
+?>
+
+<!------ Include the above in your HEAD tag ---------->
+
+<!-- Footer -->
+
 <section id="footer">
     <div class="container">
-        <div class="row text-center text-xs-center text-sm-left text-md-left">
-            <div class="col-xs-12 col-sm-4 col-md-4">
+        <div class="row  text-sm-left text-md-left">
+            <?php if ($has_sidebar_1 || $has_sidebar_2 || $has_sidebar_3) { ?>
+                <div class="col-xs-12 col-sm-4 col-md-4">
+                    <?php if ($has_sidebar_1) { ?>
+                        <div class="footer-nav-widget column-one grid-item">
+                            <?php dynamic_sidebar('sidebar-1'); ?>
+                        </div>
 
-                <h5 class="wp-block-group__inner-container">Recent Posts</h5>
-                <ul class="list-unstyled quick-links">
-                    <li><a href="http://wordpress.local/2021/09/22/solskjaer-ronaldo-khong-chi-dao-cau-thu-man-utd/">Solskjaer:
-                            ‘Ronaldo không chỉ đạo cầu thủ Man Utd’</a></li>
-                    <li><a href="http://wordpress.local/2021/09/22/test1/">Man Utd bị loại khỏi Cup Liên đoàn</a></li>
-                    <li><a href="http://wordpress.local/2021/09/16/hello-world/">Việt Nam thua sát Nga ở vòng 1/8 futsal
-                            World Cup</a></li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-4 col-md-4">
-                <h5 >Recent Comments</h5>
-                <ul class="list-unstyled quick-links">
-                    <li class="wp-block-latest-comments__comment">
-                        <a class="wp-block-latest-comments__comment-author"
-                           href="https://wordpress.org/">A WordPress Commenter</a>
-                        <a class="wp-block-latest-comments__comment-link"
-                           href="http://wordpress.local/2021/09/16/hello-world/#comment-1">Việt Nam
-                            thua sát Nga ở vòng 1/8 futsal World Cup</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-4 col-md-4">
-                <h5 >Archives</h5>
-
-                <ul class="list-unstyled quick-links">
-                    <li><a href="http://wordpress.local/2021/09/">September 2021</a></li>
-                </ul>
-            </div>
+                    <?php } ?>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4">
+                    <?php if ($has_sidebar_2) { ?>
+                        <div class="footer-nav-widget column-two grid-item">
+                            <?php dynamic_sidebar('sidebar-2'); ?>
+                        </div>
+                    <?php } ?>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4">
+                    <?php if ($has_sidebar_3) { ?>
+                        <div class="footer-nav-widget column-two grid-item">
+                            <?php dynamic_sidebar('sidebar-3'); ?>
+                        </div>
+                    <?php } ?>
+                </div>
+            <?php } ?>
         </div>
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
                 <ul class="list-unstyled list-inline social text-center">
-                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i
-                                    class="fa fa-facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i
-                                    class="fa fa-twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i
-                                    class="fa fa-instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i
-                                    class="fa fa-google-plus"></i></a></li>
-                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i
-                                    class="fa fa-envelope"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-google-plus"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></li>
                 </ul>
             </div>
             <hr>
         </div>
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-                <p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a
-                    Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis,
-                    MN]</p>
-                <p class="h6" style="font-family: 'Adamina';font-size:16px;">© All right Reversed.<a
-                            class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a>
-                </p>
+                <p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
+                <p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
             </div>
             <hr>
         </div>
     </div>
 </section>
-
 <!-- ./Footer -->
+
 <?php wp_footer(); ?>
+
+</body>
 
 </html>
