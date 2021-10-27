3c96ac9452d1f478e86b927fa195dc37f949530a
hieu294 <tranthehieu12>
2021-10-27 18:30:39 +0700

    update file

--- wp-content/themes/twentytwenty/templates/template-cover.php
+++ wp-content/themes/twentytwenty/templates/template-cover.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: Cover Template
  * Template Post Type: post, page
@@ -15,12 +16,12 @@ get_header();
 
 	<?php
 
-	if ( have_posts() ) {
+	if (have_posts()) {
 
-		while ( have_posts() ) {
+		while (have_posts()) {
 			the_post();
 
-			get_template_part( 'template-parts/content-cover' );
+			get_template_part('template-parts/content-cover');
 		}
 	}
 
@@ -28,6 +29,6 @@ get_header();
 
 </main><!-- #site-content -->
 
-<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+<?php get_template_part('template-parts/footer-menus-widgets'); ?>
 
 <?php get_footer(); ?>
