f89b3e0cd1c300abc1e508cadf3ad39226bc6036
PNTrungHau <18211tt4361@mail.tdc.edu.vn>
2021-10-29 01:10:19 +0700

    upfile

---
+++ wp-content/themes/twentytwenty/footer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+?>
+
+<!------ Include the above in your HEAD tag ---------->
+	<section id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+					<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+						<li class="list-inline-item"><a href="#" target="_blank"><i class="fa fa-envelope"></i></a></li>
+					</ul>
+				</div>
+				<hr>
+			</div>	
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					<p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
+					<p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
+				</div>
+				<hr>
+			</div>	
+		</div>
+	</section><!-- #site-footer -->
+
+	
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * The default template for displaying content
+ *
+ * Used for both singular and index.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+$class = "";
+$classes['content-format'] = "";
+if (!is_single()) {
+	$class = "danh-sach";
+	$classes['content-format'] = "content-format";
+}
+
+?>
+
+<article <?php post_class($class); ?> id="post-<?php the_ID(); ?>">
+
+	<?php
+	get_template_part('template-parts/entry-header');
+
+	if (!is_search()) {
+		get_template_part('template-parts/featured-image');
+	}
+
+	?>
+	<div class="section-inner">
+		<?php
+		wp_link_pages(
+			array(
+				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
+				'after'       => '</nav>',
+				'link_before' => '<span class="page-number">',
+				'link_after'  => '</span>',
+			)
+		);
+
+		if (is_single())
+			edit_post_link();
+
+		// Single bottom post meta.
+		twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
+
+		if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
+
+			get_template_part('template-parts/entry-author-bio');
+		}
+		?>
+
+	</div><!-- .section-inner -->
+
+	<div <?php post_class($classes['content-format']); ?> class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
+
+		<div class="entry-content">
+
+			<?php
+			if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
+				the_excerpt();
+			} else {
+				if (is_single()) {
+					the_content(__('Continue reading', 'twentytwenty'));
+				} else {
+					$post = get_post();
+					echo substr($post->post_content, 0, 100);
+				}
+			}
+			?>
+
+		</div><!-- .entry-content -->
+
+	</div><!-- .post-inner -->
+
+
+
+	<?php
+
+	if (is_single()) {
+
+		get_template_part('template-parts/navigation');
+	}
+
+	/*
+	 * Output comments wrapper if it's a post, or if comments are open,
+	 * or if there's a comment number – and check for password.
+	 */
+	if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
+	?>
+
+		<div class="comments-wrapper section-inner">
+
+			<?php comments_template(); ?>
+
+		</div><!-- .comments-wrapper -->
+
+	<?php
+	}
+	?>
+
+</article><!-- .post -->
