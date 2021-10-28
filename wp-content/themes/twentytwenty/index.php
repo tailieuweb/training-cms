072c42c68129da1ce62e74969fc15038c79bf453
VoDangVinh2000 <01686228222vinh@gmail.com>
2021-10-28 11:44:15 +0700

    update footer back to initial

--- wp-content/themes/twentytwenty/index.php
+++ wp-content/themes/twentytwenty/index.php
@@ -114,7 +114,7 @@ get_header();
 
 </main><!-- #site-content -->
 
-<?php #get_template_part( 'template-parts/footer-menus-widgets' ); 
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); 
 ?>
 
 <?php
