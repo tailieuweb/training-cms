bc06385ae97283a51851ef12ceeca34c96cf5bc4
sangtm97 <minhsangtrieu94@gmail.com>
2021-10-28 12:12:48 +0700

    Update module 2

--- wp-content/themes/twentytwenty/index.php
+++ wp-content/themes/twentytwenty/index.php
@@ -81,15 +81,46 @@ get_header();
 		$i = 0;
 
 		while ( have_posts() ) {
-			$i++;
-			if ( $i > 1 ) {
-				echo '<hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" />';
-			}
+			// $i++;
+			// if ( $i > 1 ) {
+			// 	echo '<hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" />';
+			// }
 			the_post();
+			$post = get_post();
+			//lay thong tin tu post
+			$post_title = $post->post_title;
+			$post_date = get_the_date('d', $post->ID);
+			$post_month = get_the_date('F', $post->ID);
+			$post_content = substr($post->post_content,0,150);
+			//get_template_part( 'template-parts/content', get_post_type() );
 
-			get_template_part( 'template-parts/content', get_post_type() );
-
-		}
+			//hien thi du lieu da lay
+		?>  
+			<div class="container">
+				<div class="list_new">
+					<div class='list_new_view'>
+						<div class='row'>
+							<div class='col-md top_news_block_desc'>
+								<div class='row'>
+									<div class='col-md-3 col-xs-3 topnewstime'>
+										<span class='topnewsdate'><?= $post_date ?></span><br>
+										<span class='topnewsmonth'><?= $post_month ?></span><br>
+									</div>
+									<div class='col-md-9 col-xs-9 shortdesc'>
+										<h4>
+										<a href='<?= esc_url( get_permalink() )?>'><?= $post_title ?></a>
+										</h4>
+										<?= $post_content ?> 
+										<a href='<?= esc_url( get_permalink() )?>'>[...]</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php
+		}	
 	} elseif ( is_search() ) {
 		?>
 
