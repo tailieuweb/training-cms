90083868c4d0add745ac91a9b2eff7a09746e31b
vinhvvdvGithub <vinhvvdv@gmail.com>
2021-10-28 22:18:56 +0700

    custom layout content

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The default template for displaying content
  *
@@ -16,28 +17,49 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 	<?php
-
-	get_template_part( 'template-parts/entry-header' );
-
-	if ( ! is_search() ) {
-		get_template_part( 'template-parts/featured-image' );
-	}
-
+	$post = get_post();
+	$month = date("m", strtotime($post->post_date));
+	$day = date("d", strtotime($post->post_date));
 	?>
+	<div class="container content-body">
+		<div class="col-image">
 
-	<div class="post-inner <?php echo is_page_template( 'templates/template-full-width.php' ) ? '' : 'thin'; ?> ">
+			<img src="<?php get_template_part('template-parts/featured-image'); ?>" alt="">
+		</div>
+		<div class="col-day">
+			<div class="content-col-day">
+				<?php if (!is_single()) {  ?>
+					<div class="day"><?php echo $day ?></div>
+					<div class="month">Tháng <?php echo $month ?></div>
+				<?php } ?>
+			</div>
+		</div>
+		<div class="col-content">
+			<div class="content-header">
+				<?php
+				if (!is_search()) {
+					get_template_part('template-parts/entry-header');
+				}
+				?>
+			</div>
+			<div class="entry-content">
 
-		<div class="entry-content">
+				<?php
+				if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
+					the_excerpt();
+				} else {
+					if (is_single()) {
+						the_content(__('Continue reading', 'twentytwenty'));
+					} else {
+						echo substr($post->post_content, 0, 120) . "<a href=" . $post->guid . ">[...]</a>";
+					}
+				}
+				?>
 
-			<?php
-			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
-				the_excerpt();
-			} else {
-				the_content( __( 'Continue reading', 'twentytwenty' ) );
-			}
-			?>
+			</div><!-- .entry-content -->
+		</div>
 
-		</div><!-- .entry-content -->
+	</div>
 
 	</div><!-- .post-inner -->
 
@@ -45,7 +67,7 @@
 		<?php
 		wp_link_pages(
 			array(
-				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
+				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
 				'after'       => '</nav>',
 				'link_before' => '<span class="page-number">',
 				'link_after'  => '</span>',
@@ -55,12 +77,11 @@
 		edit_post_link();
 
 		// Single bottom post meta.
-		twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
+		twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
 
-		if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
+		if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
 
-			get_template_part( 'template-parts/entry-author-bio' );
-
+			get_template_part('template-parts/entry-author-bio');
 		}
 		?>
 
@@ -68,18 +89,17 @@
 
 	<?php
 
-	if ( is_single() ) {
+	if (is_single()) {
 
-		get_template_part( 'template-parts/navigation' );
-
+		get_template_part('template-parts/navigation');
 	}
 
 	/*
-	 * Output comments wrapper if it's a post, or if comments are open,
-	 * or if there's a comment number – and check for password.
-	 */
-	if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
-		?>
+ * Output comments wrapper if it's a post, or if comments are open,
+ * or if there's a comment number – and check for password.
+ */
+	if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
+	?>
 
 		<div class="comments-wrapper section-inner">
 
@@ -87,8 +107,11 @@
 
 		</div><!-- .comments-wrapper -->
 
-		<?php
+	<?php
 	}
 	?>
+	</div>
+
+
 
 </article><!-- .post -->
