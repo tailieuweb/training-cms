733dff2b7ca33cfb200b5ebad400dfce669745e7
xuanhung3715 <19211tt3715@mail.tdc.edu.vn>
2021-10-28 09:27:19 +0700

    form comment

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -93,7 +93,36 @@ if (!is_single()) {
 
 			 <?php comments_template(); ?>
 
-			
+			<div class="aaa col-md-12">
+				<section class="card">
+					<div class="card-header">
+						<ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
+							<li class="nav-item">
+								<a class="nav-link active" id="posts-tab" data-toggle="tab" href="#posts" role="tab" aria-controls="posts" aria-selected="true">Make
+									a Post</a>
+							</li>
+						</ul>
+					</div>
+					<div class="card-body">
+						<div class="tab-content" id="myTabContent">
+							<div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
+								<div class="form-group">
+									<label class="sr-only" for="message">post</label>
+									<textarea class="form-control" id="message" rows="3" placeholder="What are you thinking..."></textarea>
+								</div>
+
+							</div>
+						</div>
+						<div class="text-right">
+							<button type="submit" class="btn btn-primary">share</button>
+						</div>
+					</div>
+				</section>
+				<!--- Post Form Ends -->
+
+				<!-- comment card ends -->
+
+			</div>
 		</div>
 
 		</div><!-- .comments-wrapper -->
