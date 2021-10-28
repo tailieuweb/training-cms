e22d4b2cceee1d5ccd2c1bb3308cba026fa52773
Tranvanlap <Tranvanlap2069@gmail.com>
2021-10-28 10:46:08 +0700

    done module-4

--- wp-content/themes/twentytwenty/searchform.php
+++ wp-content/themes/twentytwenty/searchform.php
@@ -24,6 +24,7 @@ if (empty($twentytwenty_aria_label) && !empty($args['label'])) {
 	$twentytwenty_aria_label = 'aria-label="' . esc_attr($args['label']) . '"';
 }
 ?>
+<!-- search -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <link rel="stylesheet" href="./style.css">
