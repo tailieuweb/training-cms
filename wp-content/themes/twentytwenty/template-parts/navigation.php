8108b3b16a67e0e4857cfde878f830766a77f5ae
sangtruong0317 <truonggioisang.tdc2019@gmail.com>
2021-10-28 11:04:38 +0700

    update module 7

--- wp-content/themes/twentytwenty/template-parts/navigation.php
+++ wp-content/themes/twentytwenty/template-parts/navigation.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Displays the next and previous post navigation in single posts.
  *
@@ -9,53 +10,58 @@
 
 $next_post = get_next_post();
 $prev_post = get_previous_post();
-
-if ( $next_post || $prev_post ) {
-
+$post = get_post();
+$date = $post->post_date;
+$day = date("d", strtotime($date));
+$month = date("m", strtotime($date));
+$year = date("y", strtotime($date));
+if ($next_post || $prev_post) {
 	$pagination_classes = '';
-
-	if ( ! $next_post ) {
+	if (!$next_post) {
 		$pagination_classes = ' only-one only-prev';
-	} elseif ( ! $prev_post ) {
+	} elseif (!$prev_post) {
 		$pagination_classes = ' only-one only-next';
 	}
-
-	?>
-
-	<nav class="pagination-single section-inner<?php echo esc_attr( $pagination_classes ); ?>" aria-label="<?php esc_attr_e( 'Post', 'twentytwenty' ); ?>" role="navigation">
-
-		<hr class="styled-separator is-style-wide" aria-hidden="true" />
-
+?>
+	<nav class="pagination-single section-inner<?php echo esc_attr($pagination_classes); ?>" aria-label="<?php esc_attr_e('Post', 'twentytwenty'); ?>" role="navigation">
 		<div class="pagination-single-inner">
-
 			<?php
-			if ( $prev_post ) {
-				?>
-
-				<a class="previous-post" href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>">
-					<span class="arrow" aria-hidden="true">&larr;</span>
-					<span class="title"><span class="title-inner"><?php echo wp_kses_post( get_the_title( $prev_post->ID ) ); ?></span></span>
+			if ($prev_post) {
+			?>
+				<a class="previous-post" href="<?php echo esc_url(get_permalink($prev_post->ID)); ?>">
+					<div class="time" style="
+    font-size: 0.8em;
+    width: 15%;
+    min-width: 55px;
+    display: flex;
+    vertical-align: middle;
+">
+						<div class="day"><?php echo $day . "<br>" . $month ?></div>
+						<div class="year" style="margin-top: 7%;margin-left: 5%;"><?php echo $year ?></div>
+					</div>
+					<span class="title"><span class="title-inner"><?php echo wp_kses_post(get_the_title($prev_post->ID)); ?></span></span>
 				</a>
-
-				<?php
+			<?php
 			}
-
-			if ( $next_post ) {
-				?>
-
-				<a class="next-post" href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>">
-					<span class="arrow" aria-hidden="true">&rarr;</span>
-						<span class="title"><span class="title-inner"><?php echo wp_kses_post( get_the_title( $next_post->ID ) ); ?></span></span>
+			if ($next_post) {
+			?>
+				<a class="next-post" href="<?php echo esc_url(get_permalink($next_post->ID)); ?>">
+				<div class="time" style="
+    font-size: 0.8em;
+    width: 15%;
+    min-width: 55px;
+    display: flex;
+    vertical-align: middle;
+">
+						<div class="day"><?php echo $day . "<br>" . $month ?></div>
+						<div class="year" style="margin-top: 7%;margin-left: 5%;"><?php echo $year ?></div>
+					</div>
+					<span class="title"><span class="title-inner"><?php echo wp_kses_post(get_the_title($next_post->ID)); ?></span></span>
 				</a>
-				<?php
+			<?php
 			}
 			?>
-
 		</div><!-- .pagination-single-inner -->
-
-		<hr class="styled-separator is-style-wide" aria-hidden="true" />
-
 	</nav><!-- .pagination-single -->
-
-	<?php
+<?php
 }
