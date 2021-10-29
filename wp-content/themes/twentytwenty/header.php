1eff98ae11bcf2fe012632099ab8cf41bcfb6ea0
Tan Le <19211tt1603@mail.tdc.edu.vn>
2021-10-29 09:27:45 +0700

    update header.php

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -20,7 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="profile" href="https://gmpg.org/xfn/11">
-
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/cssnhomj/module_2.css" type="text/css" media="screen" />
     <?php wp_head(); ?>
 
 </head>
