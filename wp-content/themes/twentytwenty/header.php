bbf282b4779df348432737a1534a9c931849e0ea
Nguyen Trong Tinh <trongtinh2k@gmail.com>
2021-10-29 12:31:44 +0700

    module-6

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -23,9 +23,9 @@
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <link rel="profile" href="https://gmpg.org/xfn/11">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/module-6.css" type="text/css" media="screen" />
 
-
-		<?php wp_head(); ?>
+        <?php wp_head(); ?>
 
 	</head>
 
