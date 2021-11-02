ef9c0aa976d503e99826ffd8fa7953d2f89c7915
Longkunz <Cuvip1325@gmail.com>
2021-11-02 14:25:48 +0700

    Custom show index post structure

--- wp-content/themes/twentytwenty/index.php
+++ wp-content/themes/twentytwenty/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -24,16 +25,16 @@ get_header();
 	$archive_title    = '';
 	$archive_subtitle = '';
 
-	if ( is_search() ) {
+	if (is_search()) {
 		global $wp_query;
 
 		$archive_title = sprintf(
 			'%1$s %2$s',
-			'<span class="color-accent">' . __( 'Search:', 'twentytwenty' ) . '</span>',
+			'<span class="color-accent">' . __('Search:', 'twentytwenty') . '</span>',
 			'&ldquo;' . get_search_query() . '&rdquo;'
 		);
 
-		if ( $wp_query->found_posts ) {
+		if ($wp_query->found_posts) {
 			$archive_subtitle = sprintf(
 				/* translators: %s: Number of search results. */
 				_n(
@@ -42,31 +43,31 @@ get_header();
 					$wp_query->found_posts,
 					'twentytwenty'
 				),
-				number_format_i18n( $wp_query->found_posts )
+				number_format_i18n($wp_query->found_posts)
 			);
 		} else {
-			$archive_subtitle = __( 'We could not find any results for your search. You can give it another try through the search form below.', 'twentytwenty' );
+			$archive_subtitle = __('We could not find any results for your search. You can give it another try through the search form below.', 'twentytwenty');
 		}
-	} elseif ( is_archive() && ! have_posts() ) {
-		$archive_title = __( 'Nothing Found', 'twentytwenty' );
-	} elseif ( ! is_home() ) {
+	} elseif (is_archive() && !have_posts()) {
+		$archive_title = __('Nothing Found', 'twentytwenty');
+	} elseif (!is_home()) {
 		$archive_title    = get_the_archive_title();
 		$archive_subtitle = get_the_archive_description();
 	}
 
-	if ( $archive_title || $archive_subtitle ) {
-		?>
+	if ($archive_title || $archive_subtitle) {
+	?>
 
 		<header class="archive-header has-text-align-center header-footer-group">
 
 			<div class="archive-header-inner section-inner medium">
 
-				<?php if ( $archive_title ) { ?>
-					<h1 class="archive-title"><?php echo wp_kses_post( $archive_title ); ?></h1>
+				<?php if ($archive_title) { ?>
+					<h1 class="archive-title"><?php echo wp_kses_post($archive_title); ?></h1>
 				<?php } ?>
 
-				<?php if ( $archive_subtitle ) { ?>
-					<div class="archive-subtitle section-inner thin max-percentage intro-text"><?php echo wp_kses_post( wpautop( $archive_subtitle ) ); ?></div>
+				<?php if ($archive_subtitle) { ?>
+					<div class="archive-subtitle section-inner thin max-percentage intro-text"><?php echo wp_kses_post(wpautop($archive_subtitle)); ?></div>
 				<?php } ?>
 
 			</div><!-- .archive-header-inner -->
@@ -76,21 +77,65 @@ get_header();
 		<?php
 	}
 
-	if ( have_posts() ) {
+	//Phần hiển thị posts
+	if (have_posts()) {
 
 		$i = 0;
 
-		while ( have_posts() ) {
-			$i++;
-			if ( $i > 1 ) {
-				echo '<hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" />';
-			}
+		while (have_posts()) {
 			the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			//Lấy post
+			$post = get_post();
 
+			//Lấy thông tin từ $post
+			$post_title = $post->post_title;
+			$post_date = get_the_date('d', $post->ID);
+			$post_month = get_the_date('m', $post->ID);
+			$post_content = substr(
+				$post->post_content,
+				strpos($post->post_content, "<!-- wp:paragraph -->"),
+				strpos($post->post_content, "<!-- /wp:paragraph -->")
+			);
+			$post_image = get_the_post_thumbnail($post->ID, 'thumbnail');
+			$post_guid = $post->guid;
+			$post_content = $post_content . "<a href='" . $post_guid . "'>[...]</a>";
+			// Xuất dữ liệu đã lấy
+		?>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<div class="list_new">
+							<div class='list_new_view'>
+								<div class='row'>
+									<div class="col-md-5">
+										<div class="top_news_block_thumb">
+											<?= $post_image ?>
+										</div>
+									</div>
+									<div class='col-md-7 top_news_block_desc'>
+										<div class='row'>
+											<div class='col-md-3 col-xs-3 topnewstime'>
+												<span class='topnewsdate'><?= $post_date ?></span><br>
+												<span class='topnewsmonth'><?= "Tháng " . $post_month ?></span><br>
+											</div>
+											<div class='col-md-9 col-xs-9 shortdesc'>
+												<h4>
+													<a href='<?= $post_guid ?>'><?= $post_title ?></a>
+												</h4>
+												<p><?= the_excerpt() ?></p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php
 		}
-	} elseif ( is_search() ) {
+	} elseif (is_search()) {
 		?>
 
 		<div class="no-search-results-form section-inner thin">
@@ -98,22 +143,22 @@ get_header();
 			<?php
 			get_search_form(
 				array(
-					'aria_label' => __( 'search again', 'twentytwenty' ),
+					'aria_label' => __('search again', 'twentytwenty'),
 				)
 			);
 			?>
 
 		</div><!-- .no-search-results -->
 
-		<?php
+	<?php
 	}
 	?>
 
-	<?php get_template_part( 'template-parts/pagination' ); ?>
+	<?php get_template_part('template-parts/pagination'); ?>
 
 </main><!-- #site-content -->
 
-<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+<?php get_template_part('template-parts/footer-menus-widgets'); ?>
 
 <?php
 get_footer();
