3675cb95707a9dc17a22f8b46794d6b431f54180
sangtruong0317 <truonggioisang.tdc2019@gmail.com>
2021-11-05 18:41:26 +0700

    update module 8

--- wp-content/themes/twentytwenty/comments.php
+++ wp-content/themes/twentytwenty/comments.php
@@ -107,11 +107,12 @@ if ( comments_open() || pings_open() ) {
 	}
 
 	comment_form(
-		array(
-			'class_form'         => 'section-inner thin max-percentage',
-			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
-			'title_reply_after'  => '</h2>',
-		)
+		
+		// array(
+		// 	'class_form'         => 'section-inner thin max-percentage',
+		// 	'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+		// 	'title_reply_after'  => '</h2>',
+		// )
 	);
 
 } elseif ( is_single() ) {
