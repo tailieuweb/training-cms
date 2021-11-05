784595bf0636a9de87b06383dcdcb5b6a68767c6
Tranvanlap <Tranvanlap2069@gmail.com>
2021-11-05 20:10:32 +0700

    done module-4-testby

--- wp-content/themes/twentytwenty/searchform.php
+++ wp-content/themes/twentytwenty/searchform.php
@@ -24,40 +24,13 @@ if (empty($twentytwenty_aria_label) && !empty($args['label'])) {
 	$twentytwenty_aria_label = 'aria-label="' . esc_attr($args['label']) . '"';
 }
 ?>
-<!-- search -->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<link rel="stylesheet" href="./style.css">
-<link rel="stylesheet" href="./styleSearch.css">
-<div class="container-search">
-	<div class="row justify-content-center row-seach">
-		<div class="col-12 col-md-10 col-lg-8">
-			<form role="search" <?php echo $twentytwenty_aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. 
-								?> method="get" class="search-form card card-sm" action="<?php echo esc_url(home_url('/')); ?>">
-				<div class="card-body row no-gutters align-items-center" for="<?php echo esc_attr($twentytwenty_unique_id); ?>">
-					<div class="col-auto">
-						<i class="fa fa-search fa-4x" aria-hidden="true"></i>
-					</div>
-					<div class="col">
-						<input type="search" id="<?php echo esc_attr($twentytwenty_unique_id); ?>" class="search-field form-control form-control-lg form-control-borderless" placeholder="<?php echo esc_attr_x('Search topics or keywords &hellip;', 'placeholder', 'twentytwenty'); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-					</div>
-					<div class="col-auto">
-						<button type="submit" class="search-submit btn btn-lg btn-success" value="<?php echo esc_attr_x('Search', 'submit button', 'twentytwenty'); ?>">Search</button>
-					</div>
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!-- 
-		<form role="search" <?php echo $twentytwenty_aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. 
-							?> method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
+<form role="search" <?php echo $twentytwenty_aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. 
+					?> method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
 	<label for="<?php echo esc_attr($twentytwenty_unique_id); ?>">
+		<i class="fa fa-search fa-3x" aria-hidden="true"></i>
 		<span class="screen-reader-text"><?php _e('Search for:', 'twentytwenty'); // phpcs:ignore: WordPress.Security.EscapeOutput.UnsafePrintingFunction -- core trusts translations 
 											?></span>
-		<input type="search" id="<?php echo esc_attr($twentytwenty_unique_id); ?>" class="search-field" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder', 'twentytwenty'); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+		<input type="search" id="<?php echo esc_attr($twentytwenty_unique_id); ?>" class="search-field" placeholder="<?php echo esc_attr_x('Search topics or keywords &hellip;', 'placeholder', 'twentytwenty'); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
 	<input type="submit" class="search-submit HIHI" value="<?php echo esc_attr_x('Search', 'submit button', 'twentytwenty'); ?>" />
-</form> -->
+</form>
