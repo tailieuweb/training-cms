fa4c38495f8ab3fcb8cd077e0c7bc878e3654bd3
sangtm97 <minhsangtrieu94@gmail.com>
2021-11-06 22:15:21 +0700

    update module 2

--- wp-content/themes/twentytwenty/index.php
+++ wp-content/themes/twentytwenty/index.php
@@ -19,7 +19,7 @@ get_header();
 
 <main id="site-content" role="main">
 
-	<?php
+    <?php
 
 	$archive_title    = '';
 	$archive_subtitle = '';
@@ -57,23 +57,24 @@ get_header();
 	if ( $archive_title || $archive_subtitle ) {
 		?>
 
-		<header class="archive-header has-text-align-center header-footer-group">
+    <header class="archive-header has-text-align-center header-footer-group">
 
-			<div class="archive-header-inner section-inner medium">
+        <div class="archive-header-inner section-inner medium">
 
-				<?php if ( $archive_title ) { ?>
-					<h1 class="archive-title"><?php echo wp_kses_post( $archive_title ); ?></h1>
-				<?php } ?>
+            <?php if ( $archive_title ) { ?>
+            <h1 class="archive-title"><?php echo wp_kses_post( $archive_title ); ?></h1>
+            <?php } ?>
 
-				<?php if ( $archive_subtitle ) { ?>
-					<div class="archive-subtitle section-inner thin max-percentage intro-text"><?php echo wp_kses_post( wpautop( $archive_subtitle ) ); ?></div>
-				<?php } ?>
+            <?php if ( $archive_subtitle ) { ?>
+            <div class="archive-subtitle section-inner thin max-percentage intro-text">
+                <?php echo wp_kses_post( wpautop( $archive_subtitle ) ); ?></div>
+            <?php } ?>
 
-			</div><!-- .archive-header-inner -->
+        </div><!-- .archive-header-inner -->
 
-		</header><!-- .archive-header -->
+    </header><!-- .archive-header -->
 
-		<?php
+    <?php
 	}
 
 	if ( have_posts() ) {
@@ -95,38 +96,40 @@ get_header();
 			//get_template_part( 'template-parts/content', get_post_type() );
 
 			//hien thi du lieu da lay
-		?>  
-			<div class="container">
-				<div class="list_new">
-					<div class='list_new_view'>
-						<div class='row'>
-							<div class='col-md top_news_block_desc'>
-								<div class='row'>
-									<div class='col-md-3 col-xs-3 topnewstime'>
-										<span class='topnewsdate'><?= $post_date ?></span><br>
-										<span class='topnewsmonth'><?= $post_month ?></span><br>
-									</div>
-									<div class='col-md-9 col-xs-9 shortdesc'>
-										<h4>
-										<a href='<?= esc_url( get_permalink() )?>'><?= $post_title ?></a>
-										</h4>
-										<?= $post_content ?> 
-										<a href='<?= esc_url( get_permalink() )?>'>[...]</a>
-									</div>
+		?>
+    <div class="container">
+        <div class="list_new">
+            <div class='list_new_view'>
+                <div class='row'>
+                    <div class='col-md detail-new'>
+                        <div class='row'>
+                            <?php if ( get_post() ) { ?>
+								<div class='col-md-3 col-xs-3 time_post'>
+									<span class='date_post'><?= $post_date ?></span><br>
+									<span class='month_post'><?= $post_month ?></span><br>
 								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		<?php
+								<div class='col-md-9 col-xs-9 desc_post'>
+									<h4>
+										<a href='<?= esc_url( get_permalink() )?>'><?= $post_title ?></a>
+									</h4>
+									<?= $post_content ?>
+									<a href='<?= esc_url( get_permalink() )?>'>[...]</a>
+								</div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
 		}	
 	} elseif ( is_search() ) {
 		?>
 
-		<div class="no-search-results-form section-inner thin">
+    <div class="no-search-results-form section-inner thin">
 
-			<?php
+        <?php
 			get_search_form(
 				array(
 					'aria_label' => __( 'search again', 'twentytwenty' ),
@@ -134,13 +137,13 @@ get_header();
 			);
 			?>
 
-		</div><!-- .no-search-results -->
+    </div><!-- .no-search-results -->
 
-		<?php
+    <?php
 	}
 	?>
 
-	<?php get_template_part( 'template-parts/pagination' ); ?>
+    <?php get_template_part( 'template-parts/pagination' ); ?>
 
 </main><!-- #site-content -->
 
