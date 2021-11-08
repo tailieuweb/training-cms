44feb6d7d7df82449175d845eed0265b5a35ef3b
Nguyễn Luân <nguyenluan21qaz@gmail.com>
2021-11-08 19:56:12 +0700

    footer

--- wp-content/themes/twentytwentyone/template-parts/footer/footer-widgets.php
+++ wp-content/themes/twentytwentyone/template-parts/footer/footer-widgets.php
@@ -14,3 +14,71 @@ if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 	</aside><!-- .widget-area -->
 
 <?php endif; ?>
+<div class="footer-widgets-wrapper">
+
+
+                
+                <div class="footer-widgets column-one grid-item">
+                    <div class="widget widget_block"><div class="widget-content">
+<div class="wp-block-group"><div class="wp-block-group__inner-container">
+<h2>Recent Posts</h2>
+
+
+<ul class="wp-block-latest-posts__list wp-block-latest-posts"><li><a href="http://wordpress.local/2021/09/30/pin-co-the-uon-va-co-gian-nhu-ran/">Pin có thể uốn và co giãn như rắn</a></li>
+<li><a href="http://wordpress.local/2021/09/30/tim-nguoi-gioi-cho-nhung-nghien-cuu-hang-dau-tai-vkist/">Tìm người giỏi cho những nghiên cứu hàng đầu tại VKIST</a></li>
+<li><a href="http://wordpress.local/2021/09/30/usyk-khong-muon-knock-out-joshua/">Usyk không muốn knock-out Joshua</a></li>
+<li><a href="http://wordpress.local/2021/09/30/bo-dao-nha-vao-chung-ket-futsal-world-cup/">Bồ Đào Nha vào chung kết futsal World Cup</a></li>
+<li><a href="http://wordpress.local/2021/09/30/mourinho-cham-moc-200-tran-tai-cup-chau-au/">Mourinho chạm mốc 200 trận tại Cup châu Âu</a></li>
+</ul></div></div>
+</div></div><div class="widget widget_block"><div class="widget-content">
+<div class="wp-block-group"><div class="wp-block-group__inner-container"></div></div>
+</div></div><div class="widget widget_block"><div class="widget-content">
+<div class="wp-block-group"><div class="wp-block-group__inner-container"></div></div>
+</div></div>                </div>
+
+                
+
+                
+                <div class="footer-widgets column-two grid-item">
+                    <div class="widget widget_block"><div class="widget-content">
+<div class="wp-block-group"><div class="wp-block-group__inner-container">
+<h2>Recent Comments</h2>
+
+
+<ol class="has-avatars has-dates has-excerpts wp-block-latest-comments"><li class="wp-block-latest-comments__comment"><img alt="" src="http://1.gravatar.com/avatar/d7a973c7dab26985da5f961be7b74480?s=48&amp;d=mm&amp;r=g" srcset="http://1.gravatar.com/avatar/d7a973c7dab26985da5f961be7b74480?s=96&amp;d=mm&amp;r=g 2x" class="avatar avatar-48 photo wp-block-latest-comments__comment-avatar" height="48" width="48" loading="lazy"><article><footer class="wp-block-latest-comments__comment-meta"><a class="wp-block-latest-comments__comment-author" href="https://wordpress.org/">A WordPress Commenter</a> on <a class="wp-block-latest-comments__comment-link" href="http://wordpress.local/2021/09/30/hello-world/#comment-1">Hello world!</a><time datetime="2021-09-30T22:59:54+00:00" class="wp-block-latest-comments__comment-date">September 30, 2021</time></footer><div class="wp-block-latest-comments__comment-excerpt"><p>Hi, this is a comment. To get started with moderating, editing, and deleting comments, please visit the Comments screen in…</p>
+</div></article></li></ol></div></div>
+</div></div><div class="widget widget_block"><div class="widget-content">
+<div class="wp-block-group"><div class="wp-block-group__inner-container"></div></div>
+</div></div>                </div>
+
+                                
+                <div class="footer-widgets column-three grid-item">
+                    <div class="widget widget_block"><div class="widget-content">
+<div class="wp-block-group"><div class="wp-block-group__inner-container">
+<h2>Archives</h2>
+
+
+<ul class=" wp-block-archives-list wp-block-archives">	<li><a href="http://wordpress.local/2021/09/">September 2021</a></li>
+</ul></div></div>
+</div></div><div class="widget widget_block"><div class="widget-content">
+<div class="wp-block-group"><div class="wp-block-group__inner-container"></div></div>
+</div></div>                </div>
+
+                                
+                <div class="footer-widgets column-four grid-item">
+                    <div class="widget widget_block"><div class="widget-content">
+<div class="wp-block-group"><div class="wp-block-group__inner-container">
+<h2>Categories</h2>
+
+
+<ul class="wp-block-categories-list wp-block-categories">	<li class="cat-item cat-item-4"><a href="http://wordpress.local/category/khoa-hoc/">Khoa học</a>
+</li>
+	<li class="cat-item cat-item-2"><a href="http://wordpress.local/category/the-thao/">Thể thao</a>
+</li>
+	<li class="cat-item cat-item-1"><a href="http://wordpress.local/category/uncategorized/">Uncategorized</a>
+</li>
+</ul></div></div>
+</div></div>                </div>
+
+                
+            </div>
