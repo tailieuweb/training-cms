0dd42b1958ab04b754c009931885dd86d7f11fa1
PhanNhatQuy <phannhatquy.tdc2019@gmail.com>
2021-11-08 21:41:18 +0700

    Module 10

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -22,6 +22,8 @@
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style-module1.css" type="text/css"
         media="screen" />
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css" type="text/css"
+        media="screen" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style-module3.css" type="text/css"
         media="screen" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style-module7.css" type="text/css"
