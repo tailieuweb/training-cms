000c78c43ee5e50e48e7db22de4948c91c359940
Lê Trung Hiếu <leetrunghieu001@gmail.com>
2021-11-10 01:28:31 +0700

    Update module-6 by LeTrungHieu

--- wp-content/themes/twentytwenty/template-parts/entry-header.php
+++ wp-content/themes/twentytwenty/template-parts/entry-header.php
@@ -46,13 +46,7 @@ if (is_singular()) {
 					 */
 					$show_categories = apply_filters('twentytwenty_show_categories_in_entry_header', true);
 					/* Module-6 */
-					$day = $month = $year = 0;
-					if (strtotime($post->post_date)) {
-						$timestamp = strtotime($post->post_date);
-						$day = date("d", $timestamp);
-						$month = date("m", $timestamp);
-						$year = date("y", $timestamp);
-					}
+					
 					if (true === $show_categories && has_category()) {
 					?>
 
@@ -66,9 +60,9 @@ if (is_singular()) {
 							<div class="col-md-10 col-xs-9">
 							<?php
 						}
-
+						?><?php
 						if (is_singular()) {
-							the_title('<h1 class="entry-title">', '</h1>');
+							// the_title('<h1 class="entry-title">', '</h1>');
 						} else {
 							the_title('<h2 class="entry-title heading-size-1"><a href="' . esc_url(get_permalink()) . '">', '</a></h2>');
 						}
@@ -78,25 +72,13 @@ if (is_singular()) {
 						if (is_singular()) {
 							$intro_text_width = ' small';
 						} else {
-							$intro_text_width = ' thin module-6-thin'; // Module-6
+							// $intro_text_width = ' thin module-6-thin'; // Module-6
 						}
 							?></div>
-							<div class="col-md-2 col-xs-3">
-								<div class="headlinesdate">
-									<div class="headlinesdm">
-										<div class="headlinesday"><?php echo $day ?></div>
-										<div class="headlinesmonth"><?php echo $month ?></div>
-									</div>
-									<div class="headlinesyear"><?php echo "'" . $year ?></div>
-								</div>
-							</div>
+							
 						</div>
 
-						<div class="row">
-							<div class="col-md-12">
-								<div class="overviewline"></div>
-							</div>
-						</div>
+						
 						<?php
 						if (has_excerpt() && is_singular()) {
 						?>
