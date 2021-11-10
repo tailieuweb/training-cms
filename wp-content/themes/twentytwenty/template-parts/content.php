040c769cc545d9a714679b57595cfebe336af8dc
nguyenhuuthang-tdc <nguyenhuuthang12c8@gmail.com>
2021-11-10 21:03:03 +0700

    second-commit (update layout content (100%))

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -31,7 +31,7 @@ $class = 'danh-sach';
 
     <div class="post-inner <?php echo is_page_template( 'templates/template-full-width.php' ) ? '' : 'thin'; ?> ">
 
-        <div class="entry-content">
+        <div class="entry-content right-content">
 
             <?php
 			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
