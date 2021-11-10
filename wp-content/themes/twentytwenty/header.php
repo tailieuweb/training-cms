454cb1d365d7de229cfe00ed63b2327cd0196547
ThanhAn <fxannguyen201@gmail.com>
2021-11-10 22:31:33 +0700

    Add new file sql and remove old file sql

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -14,33 +14,32 @@
 <!DOCTYPE html>
 
 <html class="no-js" <?php language_attributes(); ?>>
-	<head>
 
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
-
-		<link rel="profile" href="https://gmpg.org/xfn/11">
-		<link rel="stylesheet"
-		href="<?php echo get_template_directory_uri() ?>/style-module9.css"
-		type="text/css" media="screen" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,700,800|Open+Sans+Condensed:300,700|Prata&amp;subset=vietnamese" rel="stylesheet" type="text/css">
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-		<?php wp_head(); ?>
 <head>
 
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel="profile" href="https://gmpg.org/xfn/11">
-  <!-- Them FA -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style-module9.css" type="text/css" media="screen" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,700,800|Open+Sans+Condensed:300,700|Prata&amp;subset=vietnamese" rel="stylesheet" type="text/css">
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/pagesearch.css">
   <?php wp_head(); ?>
 
-</head>
+  <head>
+
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <!-- Them FA -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,700,800|Open+Sans+Condensed:300,700|Prata&amp;subset=vietnamese" rel="stylesheet" type="text/css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/pagesearch.css">
+    <?php wp_head(); ?>
+  </head>
 
 <body <?php body_class(); ?>>
 
