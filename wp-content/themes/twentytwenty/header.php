fa572dc1579d4bc5bc79dca809573a03f9e5adbe
hieu294 <tranthehieu12>
2021-11-10 10:57:47 +0700

    Module10 90%

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -19,6 +19,9 @@
 
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet"
+		href="<?php echo get_template_directory_uri() ?>/style-module10.css"
+		type="text/css" media="screen" />
 
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
--- wp-content/themes/twentytwenty/template-parts/entry-header.php
+++ wp-content/themes/twentytwenty/template-parts/entry-header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Displays the post header
  *
@@ -6,70 +7,158 @@
  * @subpackage Twenty_Twenty
  * @since Twenty Twenty 1.0
  */
-
+get_header();
+$has_sidebar_9 = is_active_sidebar('sidebar-9');
 $entry_header_classes = '';
 
-if ( is_singular() ) {
+if (is_singular()) {
 	$entry_header_classes .= ' header-footer-group';
 }
-
+if (is_single()) {
+	$class = 'footer-widgets-wrapper1';
+}
+$type_post = '';
+if (is_single()) {
+	$type_post = 'single-top';
+} else {
+	$type_post = 'single-date';
+}
 ?>
 
-<header class="entry-header has-text-align-center<?php echo esc_attr( $entry_header_classes ); ?>">
-
-	<div class="entry-header-inner section-inner medium">
-
-		<?php
-		/**
-		 * Allow child themes and plugins to filter the display of the categories in the entry header.
-		 *
-		 * @since Twenty Twenty 1.0
-		 *
-		 * @param bool Whether to show the categories in header. Default true.
-		 */
-		$show_categories = apply_filters( 'twentytwenty_show_categories_in_entry_header', true );
-
-		if ( true === $show_categories && has_category() ) {
-			?>
-
-			<div class="entry-categories">
-				<span class="screen-reader-text"><?php _e( 'Categories', 'twentytwenty' ); ?></span>
-				<div class="entry-categories-inner">
-					<?php the_category( ' ' ); ?>
-				</div><!-- .entry-categories-inner -->
-			</div><!-- .entry-categories -->
-
-			<?php
-		}
-
-		if ( is_singular() ) {
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		} else {
-			the_title( '<h2 class="entry-title heading-size-1"><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
-		}
-
-		$intro_text_width = '';
-
-		if ( is_singular() ) {
-			$intro_text_width = ' small';
-		} else {
-			$intro_text_width = ' thin';
-		}
-
-		if ( has_excerpt() && is_singular() ) {
-			?>
-
-			<div class="intro-text section-inner max-percentage<?php echo $intro_text_width; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?>">
-				<?php the_excerpt(); ?>
+<header class="entry-header has-text-align-center<?php echo esc_attr($entry_header_classes); ?>">
+	<?php if (is_single()) { ?>
+		<div class="row">
+			<div class="col-md-3 module9">
+			<?php } ?>
+			<?php if ($has_sidebar_9 && is_single()) { ?>
+				<div class="footer-widgets-wrapper9">
+					<?php if ($has_sidebar_9 && is_single()) { ?>
+						<div class="footer-widgets1 column-one grid-item">
+							<?php dynamic_sidebar('sidebar-9'); ?>
+						<?php } ?>
+						</div>
+				</div>
+			<?php }
+			if (is_single()) { ?>
 			</div>
+			<div class="col-md-6" style="background: #f5f5f5;">
+			<?php } ?>
 
-			<?php
-		}
+			<div class="entry-header-inner section-inner medium">
+				<?php
+				if (!is_single()) {
 
-		// Default to displaying the post meta.
-		twentytwenty_the_post_meta( get_the_ID(), 'single-top' );
-		?>
+					// Default to displaying the post meta.
+					twentytwenty_the_post_meta(get_the_ID(), $type_post);
+				}
+				?>
 
-	</div><!-- .entry-header-inner -->
+				<div class="entry-header-inner section-inner-content">
+
+					<?php
+					/**
+					 * Allow child themes and plugins to filter the display of the categories in the entry header.
+					 *
+					 * @since Twenty Twenty 1.0
+					 *
+					 * @param bool Whether to show the categories in header. Default true.
+					 */
+					$show_categories = apply_filters('twentytwenty_show_categories_in_entry_header', true);
+
+					if (true === $show_categories && has_category() && $type_post === 'single-top') {
+					?>
+
+						<div class="entry-categories">
+							<span class="screen-reader-text"><?php _e('Categories', 'twentytwenty'); ?></span>
+						</div><!-- .entry-categories -->
+						<hr width="100%" style="text-align: center;" />
+					<?php
+					}
+
+					if (is_singular()) {
+						the_title('<h1 class="entry-title">', '</h1>');
+					} else {
+						the_title('<h2 class="entry-title heading-size-1"><a href="' . esc_url(get_permalink()) . '">', '</a></h2>');
+					}
+					?>
+
+					<?php if (is_single()) {
+						$day = $month = $year = 0;
+						if (strtotime($post->post_date)) {
+							$timestamp = strtotime($post->post_date);
+							$day = date("d", $timestamp);
+							$month = date("m", $timestamp);
+							$year = date("y", $timestamp);
+						} ?>
+
+						<div class="headlinesdate">
+							<div class="headlinesdm">
+								<div class="headlinesday">
+									<?php echo $day ?>
+								</div>
+								<div class="headlinesmonth">
+									<?php echo $month ?>
+								</div>
+							</div>
+							<div class="headlinesyear">
+								<?php echo $year ?>
+							</div>
+						</div>
+					<?php } ?>
+
+					<?php
+					$intro_text_width = '';
+
+					if (is_singular()) {
+						$intro_text_width = ' small';
+					} else {
+						$intro_text_width = ' thin';
+					}
+
+					if (has_excerpt() && is_singular()) {
+					?>
+
+						<div class="intro-text section-inner max-percentage<?php echo $intro_text_width; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output 
+																			?>">
+							<?php the_excerpt(); ?>
+						</div>
+
+
+					<?php
+					}
+					?>
+					<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
+
+						<div class="entry-content">
+
+							<?php
+							if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
+								the_excerpt();
+							} else {
+								if (is_single()) {
+									the_content(__('Continue reading', 'twentytwenty'));
+								} else {
+									the_excerpt();
+								}
+							}
+							?>
+						</div><!-- .entry-content -->
+
+					</div><!-- .post-inner -->
+				</div> <!-- .entry-header-inner-content -->
+
+			</div><!-- .entry-header-inner -->
+			<?php if (is_single()) { ?>
+			</div>
+		</div>
+		<div class="col-md-3">
+			
+		</div>
+	<?php } ?>
+
+	</div>
+
+
+
 
 </header><!-- .entry-header -->
