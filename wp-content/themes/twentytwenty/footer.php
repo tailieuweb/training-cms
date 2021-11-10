ba184d622971848067ca8ab3ae8d1cd8ba3f73a4
hosihung <bacthaychemgio11@gmail.com>
2021-11-10 17:53:01 +0700

    Change data in second type in front end

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -23,7 +23,7 @@
                                     <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-google-plus"></i></a></li>
                                     <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></li>
                                 </ul>
-                            </div>
+                            </div> 
                         </div>
                         <div class="row">
                             <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
