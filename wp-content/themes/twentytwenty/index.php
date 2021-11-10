587923f54a90c7e9eb9d3fcb32bbd9ab14e76c0f
ngoquangkhoanqkey <ngoquangkhoanqk@gmail.com>
2021-11-10 23:57:16 +0700

    commit

--- wp-content/themes/twentytwenty/index.php
+++ wp-content/themes/twentytwenty/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -24,16 +25,16 @@ get_header();
 	$archive_title    = '';
 	$archive_subtitle = '';
 
-	if ( is_search() ) {
+	if (is_search()) {
 		global $wp_query;
 
 		$archive_title = sprintf(
 			'%1$s %2$s',
-			'<span class="color-accent">' . __( 'Search:', 'twentytwenty' ) . '</span>',
+			'<span class="color-accent">' . __('Search:', 'twentytwenty') . '</span>',
 			'&ldquo;' . get_search_query() . '&rdquo;'
 		);
 
-		if ( $wp_query->found_posts ) {
+		if ($wp_query->found_posts) {
 			$archive_subtitle = sprintf(
 				/* translators: %s: Number of search results. */
 				_n(
@@ -42,31 +43,31 @@ get_header();
 					$wp_query->found_posts,
 					'twentytwenty'
 				),
-				number_format_i18n( $wp_query->found_posts )
+				number_format_i18n($wp_query->found_posts)
 			);
 		} else {
-			$archive_subtitle = __( 'We could not find any results for your search. You can give it another try through the search form below.', 'twentytwenty' );
+			$archive_subtitle = __('We could not find any results for your search. You can give it another try through the search form below.', 'twentytwenty');
 		}
-	} elseif ( is_archive() && ! have_posts() ) {
-		$archive_title = __( 'Nothing Found', 'twentytwenty' );
-	} elseif ( ! is_home() ) {
+	} elseif (is_archive() && !have_posts()) {
+		$archive_title = __('Nothing Found', 'twentytwenty');
+	} elseif (!is_home()) {
 		$archive_title    = get_the_archive_title();
 		$archive_subtitle = get_the_archive_description();
 	}
 
-	if ( $archive_title || $archive_subtitle ) {
-		?>
+	if ($archive_title || $archive_subtitle) {
+	?>
 
 		<header class="archive-header has-text-align-center header-footer-group">
 
 			<div class="archive-header-inner section-inner medium">
 
-				<?php if ( $archive_title ) { ?>
-					<h1 class="archive-title"><?php echo wp_kses_post( $archive_title ); ?></h1>
+				<?php if ($archive_title) { ?>
+					<h1 class="archive-title"><?php echo wp_kses_post($archive_title); ?></h1>
 				<?php } ?>
 
-				<?php if ( $archive_subtitle ) { ?>
-					<div class="archive-subtitle section-inner thin max-percentage intro-text"><?php echo wp_kses_post( wpautop( $archive_subtitle ) ); ?></div>
+				<?php if ($archive_subtitle) { ?>
+					<div class="archive-subtitle section-inner thin max-percentage intro-text"><?php echo wp_kses_post(wpautop($archive_subtitle)); ?></div>
 				<?php } ?>
 
 			</div><!-- .archive-header-inner -->
@@ -76,44 +77,95 @@ get_header();
 		<?php
 	}
 
-	if ( have_posts() ) {
+	//Phần hiển thị posts
+	if (have_posts()) {
 
 		$i = 0;
 
-		while ( have_posts() ) {
-			$i++;
-			if ( $i > 1 ) {
-				echo '<hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" />';
-			}
+		while (have_posts()) {
 			the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			//Lấy post
+			$post = get_post();
 
-		}
-	} elseif ( is_search() ) {
-		?>
-
-		<div class="no-search-results-form section-inner thin">
-
-			<?php
-			get_search_form(
-				array(
-					'aria_label' => __( 'search again', 'twentytwenty' ),
-				)
+			//Lấy thông tin từ $post
+			$post_title = $post->post_title;
+			$post_date = get_the_date('d', $post->ID);
+			$post_month = get_the_date('m', $post->ID);
+			$post_content = substr(
+				$post->post_content,
+				strpos($post->post_content, "<!-- wp:paragraph -->"),
+				strpos($post->post_content, "<!-- /wp:paragraph -->")
 			);
-			?>
-
+			$post_image = get_the_post_thumbnail($post->ID, 'thumbnail');
+			$post_guid = $post->guid;
+			$post_content = $post_content . "<a href='" . $post_guid . "'>[...]</a>";
+			// Xuất dữ liệu đã lấy
+		?>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<div class="list_new">
+							<div class='list_new_view'>
+								<div class='row'>
+									<div class="col-md-5">
+										<div class="top_news_block_thumb">
+											<?= $post_image ?>
+										</div>
+									</div>
+									<div class='col-md-7 top_news_block_desc'>
+										<div class='row'>
+											<div class='col-md-3 col-xs-3 topnewstime'>
+												<span class='topnewsdate'><?= $post_date ?></span><br>
+												<span class='topnewsmonth'><?= "Tháng " . $post_month ?></span><br>
+											</div>
+											<div class='col-md-9 col-xs-9 shortdesc'>
+												<h4>
+													<a href='<?= $post_guid ?>'><?= $post_title ?></a>
+												</h4>
+												<p><?= the_excerpt() ?></p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php
+		}
+	} elseif (is_search()) {
+		?>
+		<!-- Phần hiển thị form tìm kiếm khi không có kết quả -->
+		<div class="no-search-results-form section-inner thin">
+			<form class="card card-sm" method="get" action="<?php echo home_url('/'); ?>">
+				<div class="card-body row no-gutters align-items-center">
+					<div class="col-auto">
+						<i class="fas fa-search h4 text-body"></i>
+					</div>
+					<!--end of col-->
+					<div class="col">
+						<input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Search topics or keywords" name="s" id="search" value="<?php the_search_query(); ?>">
+					</div>
+					<!--end of col-->
+					<div class="col-auto">
+						<button class="btn btn-lg btn-success" type="submit">Search</button>
+					</div>
+					<!--end of col-->
+				</div>
+			</form>
 		</div><!-- .no-search-results -->
 
-		<?php
+	<?php
 	}
 	?>
 
-	<?php get_template_part( 'template-parts/pagination' ); ?>
+	<?php get_template_part('template-parts/pagination'); ?>
 
 </main><!-- #site-content -->
 
-<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+<?php get_template_part('template-parts/footer-menus-widgets'); ?>
 
 <?php
 get_footer();
--- wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
+++ wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Displays the menus and widgets at the end of the main element.
  * Visually, this output is presented as part of the footer element.
@@ -8,20 +9,22 @@
  * @since Twenty Twenty 1.0
  */
 
-$has_footer_menu = has_nav_menu( 'footer' );
-$has_social_menu = has_nav_menu( 'social' );
+$has_footer_menu = has_nav_menu('footer');
+$has_social_menu = has_nav_menu('social');
 
-$has_sidebar_1 = is_active_sidebar( 'sidebar-1' );
-$has_sidebar_2 = is_active_sidebar( 'sidebar-2' );
-$has_sidebar_3 = is_active_sidebar( 'sidebar-3' );
+$has_sidebar_1 = is_active_sidebar('sidebar-1');
+$has_sidebar_2 = is_active_sidebar('sidebar-2');
+$has_sidebar_3 = is_active_sidebar('sidebar-3');
+
 
 // Only output the container if there are elements to display.
-if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 || $has_sidebar_3) {
-	?>
+if ($has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2) {
+?>
+	<section id="footer">
+		<div class="container">
+			<!-- <div class="footer-nav-widgets-wrapper header-footer-group"> -->
 
-	<div class="footer-nav-widgets-wrapper header-footer-group">
-
-		<div class="footer-inner section-inner">
+			<!-- <div class="footer-inner section-inner"> -->
 
 			<?php
 
@@ -30,12 +33,13 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ||
 			$footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
 			$footer_top_classes .= $has_social_menu ? ' has-social-menu' : '';
 
-			if ( $has_footer_menu || $has_social_menu ) {
-				?>
-				<div class="footer-top<?php echo $footer_top_classes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?>">
-					<?php if ( $has_footer_menu ) { ?>
+			if ($has_footer_menu || $has_social_menu) {
+			?>
+				<div class="footer-top<?php echo $footer_top_classes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output 
+										?>">
+					<?php if ($has_footer_menu) { ?>
 
-						<nav aria-label="<?php esc_attr_e( 'Footer', 'twentytwenty' ); ?>" role="navigation" class="footer-menu-wrapper">
+						<nav aria-label="<?php esc_attr_e('Footer', 'twentytwenty'); ?>" role="navigation" class="footer-menu-wrapper">
 
 							<ul class="footer-menu reset-list-style">
 								<?php
@@ -53,9 +57,9 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ||
 						</nav><!-- .site-nav -->
 
 					<?php } ?>
-					<?php if ( $has_social_menu ) { ?>
+					<?php if ($has_social_menu) { ?>
 
-						<nav aria-label="<?php esc_attr_e( 'Social links', 'twentytwenty' ); ?>" class="footer-social-wrapper">
+						<nav aria-label="<?php esc_attr_e('Social links', 'twentytwenty'); ?>" class="footer-social-wrapper">
 
 							<ul class="social-menu footer-social reset-list-style social-icons fill-children-current-color">
 
@@ -85,54 +89,45 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ||
 
 			<?php } ?>
 
-<<<<<<< HEAD
-			<?php if ( $has_sidebar_1 || $has_sidebar_2 || $has_sidebar_3 ) { ?>
-=======
-			<?php if ( $has_sidebar_1 || $has_sidebar_2 || $has_sidebar_3) { ?>
->>>>>>> origin/1-wordpress-581-202109/2-groups/1-J/master
+			<?php if ($has_sidebar_1 || $has_sidebar_2) { ?>
 
-				<aside class="footer-widgets-outer-wrapper" role="complementary">
+				<!-- <aside class="footer-widgets-outer-wrapper" role="complementary"> -->
 
-					<div class="footer-widgets-wrapper">
+				<!-- <div class="footer-widgets-wrapper"> -->
+				<div class="row text-center text-xs-center text-sm-left text-md-left">
+					<?php if ($has_sidebar_1) { ?>
 
-						<?php if ( $has_sidebar_1 ) { ?>
+						<!-- <div class="footer-widgets column-one grid-item"> -->
+						<div class="col-xs-12 col-sm-4 col-md-4">
+							<?php dynamic_sidebar('sidebar-1'); ?>
+						</div>
 
-							<div class="footer-widgets column-one grid-item">
-								<?php dynamic_sidebar( 'sidebar-1' ); ?>
-							</div>
+					<?php } ?>
 
-						<?php } ?>
+					<?php if ($has_sidebar_2) { ?>
 
-						<?php if ( $has_sidebar_2 ) { ?>
+						<!-- <div class="footer-widgets column-two grid-item"> -->
+						<div class="col-xs-12 col-sm-4 col-md-4">
+							<?php dynamic_sidebar('sidebar-2'); ?>
+						</div>
 
-							<div class="footer-widgets column-two grid-item">
-								<?php dynamic_sidebar( 'sidebar-2' ); ?>
-							</div>
+					<?php } ?>
+					<?php if ($has_sidebar_3) { ?>
 
-						<?php } ?>
-						<?php if ( $has_sidebar_3 ) { ?>
+						<!-- <div class="footer-widgets column-three grid-item"> -->
+						<div class="col-xs-12 col-sm-4 col-md-4">
+							<?php dynamic_sidebar('sidebar-3'); ?>
+						</div>
 
-							<div class="footer-widgets column-two grid-item">
-								<?php dynamic_sidebar( 'sidebar-3' ); ?>
-							</div>
+					<?php } ?>
+				</div>
+		</div><!-- .footer-widgets-wrapper -->
 
-<?php } ?>
+		</aside><!-- .footer-widgets-outer-wrapper -->
 
-						<?php if ( $has_sidebar_3 ) { ?>
+	<?php } ?>
 
-							<div class="footer-widgets column-three grid-item">
-								<?php dynamic_sidebar( 'sidebar-3' ); ?>
-							</div>
-
-						<?php } ?>
-
-					</div><!-- .footer-widgets-wrapper -->
-
-				</aside><!-- .footer-widgets-outer-wrapper -->
-
-			<?php } ?>
-
-		</div><!-- .footer-inner -->
+	</div><!-- .footer-inner -->
 
 	</div><!-- .footer-nav-widgets-wrapper -->
 
