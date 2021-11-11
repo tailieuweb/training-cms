e168c5cce130e295b471af44c6b3446bd50d1c80
TuanAnhTT8 <tuanh061020@gmail.com>
2021-11-12 01:50:28 +0700

    Commited 2 files

--- wp-content/themes/twentytwenty/template-parts/content-cover.php
+++ wp-content/themes/twentytwenty/template-parts/content-cover.php
@@ -166,11 +166,10 @@
 	 * Output comments wrapper if it's a post, or if comments are open,
 	 * or if there's a comment number – and check for password.
 	 */
-	if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
+	if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() && is_user_logged_in()) {		
 		?>
 
 		<div class="comments-wrapper section-inner">
-
 			<?php comments_template(); ?>
 
 		</div><!-- .comments-wrapper -->
