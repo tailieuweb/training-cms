89172bba04cbc5c36839096ba55edcde3811b448
Daovanchung <56217660+Daovanchung@users.noreply.github.com>
2021-11-11 17:47:07 +0700

    commit module7

--- wp-content/themes/twentytwenty/template-parts/navigation.php
+++ wp-content/themes/twentytwenty/template-parts/navigation.php
@@ -19,32 +19,63 @@ if ( $next_post || $prev_post ) {
 	} elseif ( ! $prev_post ) {
 		$pagination_classes = ' only-one only-next';
 	}
-
 	?>
-
-	<nav class="pagination-single section-inner<?php echo esc_attr( $pagination_classes ); ?>" aria-label="<?php esc_attr_e( 'Post', 'twentytwenty' ); ?>" role="navigation">
-
-		<hr class="styled-separator is-style-wide" aria-hidden="true" />
-
+	<nav class="pagination-single section-inner justify-content-center<?php echo esc_attr( $pagination_classes ); ?>" aria-label="<?php esc_attr_e( 'Post', 'twentytwenty' ); ?>" role="navigation">
 		<div class="pagination-single-inner">
-
+			
 			<?php
 			if ( $prev_post ) {
 				?>
-
-				<a class="previous-post" href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>">
-					<span class="arrow" aria-hidden="true">&larr;</span>
-					<span class="title"><span class="title-inner"><?php echo wp_kses_post( get_the_title( $prev_post->ID ) ); ?></span></span>
+				<a class="previous-post" href="<?php echo esc_url( get_permalink( $prev_post->ID ) );?>">
+				<?php
+						//nếu là trang detail thì hiển thị thời gain
+						if (is_single()) {
+							echo '<div class="date-time-post">';
+							$post_date = get_the_date('d', $post->ID);
+							$post_month = get_the_date('m', $post->ID);
+							$post_year = get_the_date('y',$post->ID);
+							echo '<div class="post-date">';
+							echo '<div class="head-dm">';
+							echo '<div class="day">' . $post_date . '</div>';
+							echo '<hr style="margin: 0;">';
+							echo '<div class="month">' . $post_month . '</div>';
+							echo '</div>';
+							echo '<div class="year">' . $post_year . '</div>';
+							echo '</div>';
+							echo '</div>';
+						}
+				?>
+					<span class="title"><span class="title-inner"><?php
+				
+					echo wp_kses_post( get_the_title( $prev_post->ID ) ); ?></span></span>
 				</a>
 
 				<?php
-			}
+				
+			 }
 
+			 
 			if ( $next_post ) {
-				?>
-
+				?>	
 				<a class="next-post" href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>">
-					<span class="arrow" aria-hidden="true">&rarr;</span>
+				<?php
+						//nếu là trang detail thì hiển thị thời gain
+						if (is_single()) {
+							echo '<div class="date-time-post">';
+							$post_date = get_the_date('d', $post->ID);
+							$post_month = get_the_date('m', $post->ID);
+							$post_year = get_the_date('y',$post->ID);
+							echo '<div class="post-date">';
+							echo '<div class="head-dm">';
+							echo '<div class="day">' . $post_date . '</div>';
+							echo '<hr style="margin: 0;">';
+							echo '<div class="month">' . $post_month . '</div>';
+							echo '</div>';
+							echo '<div class="year">' . $post_year . '</div>';
+							echo '</div>';
+							echo '</div>';
+						}
+				?>
 						<span class="title"><span class="title-inner"><?php echo wp_kses_post( get_the_title( $next_post->ID ) ); ?></span></span>
 				</a>
 				<?php
@@ -53,7 +84,7 @@ if ( $next_post || $prev_post ) {
 
 		</div><!-- .pagination-single-inner -->
 
-		<hr class="styled-separator is-style-wide" aria-hidden="true" />
+		<!--<hr class="styled-separator is-style-wide" aria-hidden="true" />-->
 
 	</nav><!-- .pagination-single -->
 
