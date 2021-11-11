d22705b8cf01762916a2999e208ec1a2d7c35118
Hoang Vu <phanvu000666@gmail.com>
2021-11-11 22:45:09 +0700

    Done module#7

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -24,7 +24,7 @@
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css" type="text/css" media="screen" />
 
 	<?php wp_head(); ?>
 
--- wp-content/themes/twentytwenty/template-parts/navigation.php
+++ wp-content/themes/twentytwenty/template-parts/navigation.php
@@ -1,4 +1,6 @@
+
 <?php
+
 /**
  * Displays the next and previous post navigation in single posts.
  *
@@ -9,53 +11,61 @@
 
 $next_post = get_next_post();
 $prev_post = get_previous_post();
-
-if ( $next_post || $prev_post ) {
-
+$post = get_post();
+$date = $post->post_date;
+$day = date("d", strtotime($date));
+$month = date("m", strtotime($date));
+$year = date("y", strtotime($date));
+if ($next_post || $prev_post) {
 	$pagination_classes = '';
-
-	if ( ! $next_post ) {
+	if (!$next_post) {
 		$pagination_classes = ' only-one only-prev';
-	} elseif ( ! $prev_post ) {
+	} elseif (!$prev_post) {
 		$pagination_classes = ' only-one only-next';
 	}
-
-	?>
-
-	<nav class="pagination-single section-inner<?php echo esc_attr( $pagination_classes ); ?>" aria-label="<?php esc_attr_e( 'Post', 'twentytwenty' ); ?>" role="navigation">
-
-		<hr class="styled-separator is-style-wide" aria-hidden="true" />
-
-		<div class="pagination-single-inner">
-
-			<?php
-			if ( $prev_post ) {
-				?>
-
-				<a class="previous-post" href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>">
-					<span class="arrow" aria-hidden="true">&larr;</span>
-					<span class="title"><span class="title-inner"><?php echo wp_kses_post( get_the_title( $prev_post->ID ) ); ?></span></span>
-				</a>
-
-				<?php
-			}
-
-			if ( $next_post ) {
-				?>
-
-				<a class="next-post" href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>">
-					<span class="arrow" aria-hidden="true">&rarr;</span>
-						<span class="title"><span class="title-inner"><?php echo wp_kses_post( get_the_title( $next_post->ID ) ); ?></span></span>
-				</a>
-				<?php
-			}
-			?>
-
-		</div><!-- .pagination-single-inner -->
-
-		<hr class="styled-separator is-style-wide" aria-hidden="true" />
-
-	</nav><!-- .pagination-single -->
-
+?>
+<div class="list_news<?php echo esc_attr($pagination_classes); ?>" aria-label="<?php esc_attr_e('Post', 'twentytwenty'); ?>" role="navigation">
+    <div class="headlines">
 	<?php
+			if ($prev_post) {
+			?>
+        <ul>
+            <li>
+			<div class="headlinesdate">
+                                                <div class="headlinesdm">
+                            <div class="headlinesday"><?php echo $day ?></div>
+                            <div class="headlinesmonth"><?php echo $month ?></div>
+                        </div>
+                        <div class="headlinesyear"><?php echo $year ?></div>
+                    </div>
+                    <div class="headlinestitle">
+                        <a class="previous-post" href="<?php echo esc_url(get_permalink($prev_post->ID)); ?>">
+						<span class="title"><span class="title-inner"><?php echo wp_kses_post(get_the_title($prev_post->ID)); ?></span></span>
+					</a>                    </div>
+            </li>
+			<?php
+			}
+			if ($next_post) {
+			?> 
+			<li>
+			<div class="headlinesdate">
+                                                <div class="headlinesdm">
+                            <div class="headlinesday"><?php echo $day ?></div>
+                            <div class="headlinesmonth"><?php echo $month ?></div>
+                        </div>
+                        <div class="headlinesyear"><?php echo $year ?></div>
+                    </div>
+                    <div class="headlinestitle">
+					<a class="next-post" href="<?php echo esc_url(get_permalink($next_post->ID)); ?>">
+				
+					<span class="title"><span class="title-inner"><?php echo wp_kses_post(get_the_title($next_post->ID)); ?></span></span>
+				</a>                   </div>
+            </li>  
+			<?php
+			}
+			?>                                                                                
+        </ul>
+    </div>
+</div>
+<?php
 }
