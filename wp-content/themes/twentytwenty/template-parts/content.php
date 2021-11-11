dd9e9d40a40e540ddc3be3587c7463f8b9e52a21
chiennguyen040300 <chiennguyen040300@gmail.com>
2021-11-12 02:04:57 +0700

    done module2

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -11,14 +11,14 @@
  * @subpackage Twenty_Twenty
  * @since Twenty Twenty 1.0
  */
+$post = get_post();
+$date = $post->post_date;
+$day = date("j", strtotime($date));
+$month = date("m", strtotime($date));
+$content = findContentOrImageTag($post->post_content, 'p');
+$content = substr($content, 0, 150);
 if (is_search()) {
-	$post = get_post();
-	$date = $post->post_date;
-	$day = date("j", strtotime($date));
-	$month = date("m", strtotime($date));
-	$content = findContentOrImageTag($post->post_content,'p');
-	$content = substr($content, 0, 150);
-	$img = findContentOrImageTag($post->post_content,'img');
+	$img = findContentOrImageTag($post->post_content, 'img');
 	$img = $img ? $img : '<img src="https://vnpi-hcm.vn/wp-content/uploads/2018/01/no-image-800x600.png" alt="">';
 ?>
 	<div class="container">
@@ -30,23 +30,23 @@ if (is_search()) {
 						<div class="row">
 							<div class="col-md-5">
 								<div class="top_news_block_thumb">
-									<?= $img?>
+									<?= $img ?>
 								</div>
 							</div>
 							<div class="col-md-7 top_news_block_desc">
 								<div class="row">
 									<div class="col-md-3 col-xs-3 topnewstime">
-										<span class="topnewsdate"><?= $day?></span><br>
-										<span class="topnewsmonth">Tháng <?= $month?></span><br>
+										<span class="topnewsdate-module-5"><?= $day ?></span><br>
+										<span class="topnewsmonth-module-5">Tháng <?= $month ?></span><br>
 									</div>
 									<div class="col-md-9 col-xs-9 shortdesc">
 										<h4>
-											<a href="<?= $post->guid?>">
-												<?= $post->post_title?>
+											<a href="<?= $post->guid ?>" class="post-title-model-5">
+												<?= $post->post_title ?>
 											</a>
 										</h4>
-										<p><?= $content?><a href="<?= $post->guid?>">[...]</a>
-										</p>
+										<div class="post-content-model-5"><?= $content ?><a href="<?= $post->guid ?>">[...]</a>
+										</div>
 									</div>
 
 								</div>
@@ -61,83 +61,35 @@ if (is_search()) {
 <?php
 } else if (!is_single()) {
 ?>
-	}
-	<article <?php post_class('danhsach'); ?> id="post-<?php the_ID(); ?>" style="background: url(../assets/images/bg_pattern.png) repeat;">
-		<?php
-
-		get_template_part('template-parts/entry-header');
-
-		if (!is_search()) {
-			get_template_part('template-parts/featured-image');
-		}
-
-		?>
-
-		<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
-
-			<div class="entry-content">
-
-				<?php
-				if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
-					the_excerpt();
-				} else {
-					the_content(__('Continue reading', 'twentytwenty'));
-				}
-				?>
-
-			</div><!-- .entry-content -->
-
-		</div><!-- .post-inner -->
-
-		<div class="section-inner">
-			<?php
-			wp_link_pages(
-				array(
-					'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
-					'after'       => '</nav>',
-					'link_before' => '<span class="page-number">',
-					'link_after'  => '</span>',
-				)
-			);
-
-			edit_post_link();
-
-			// Single bottom post meta.
-			twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
-
-			if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
-
-				get_template_part('template-parts/entry-author-bio');
-			}
-			?>
-
-		</div><!-- .section-inner -->
-
-		<?php
-
-		if (is_single()) {
-
-			get_template_part('template-parts/navigation');
-		}
-
-		/*
-	 * Output comments wrapper if it's a post, or if comments are open,
-	 * or if there's a comment number – and check for password.
-	 */
-		if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
-		?>
-
-			<div class="comments-wrapper section-inner">
-
-				<?php comments_template(); ?>
-
-			</div><!-- .comments-wrapper -->
-
-		<?php
-		}
-		?>
-
-	</article><!-- .post -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-6">
+				<div class="list_news">
+					<div class="list_new_view">
+						<div class="top_news_block_desc">
+							<div class="row">
+								<div class="col-md-3 col-xs-3 topnewstime">
+									<span class="topnewsdate-module-2"><?= $day ?></span><br>
+									<span class="topnewsmonth-module-2">Tháng <?= $month ?></span><br>
+								</div>
+								<div class="col-md-9 col-xs-9 shortdesc">
+									<h4>
+										<a href="<?= $post->guid ?>" class="post-title-module">
+											<?= $post->post_title ?>
+										</a>
+									</h4>
+									<div class="post-content-module"><?= $content ?><a href="<?= $post->guid ?>">[...]</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3"></div>
+		</div>
+	</div>
 <?php
 } else {
 ?>
