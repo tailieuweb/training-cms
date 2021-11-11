d83139cd1c1a0936c4cc270cc850dc2c984de829
Duc Manh <c9hai120@gmail.com>
2021-11-11 08:02:16 +0700

    Update search result

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -158,9 +158,6 @@ if (!is_search()) {
 				$post = get_post();
 				$date = date('d',strtotime($post->post_date));
 				$month = date('m',strtotime($post->post_date));
-				// // var_dump($date);
-				// var_dump($post);
-				//die();
 		?>
 				<div class="list_news">
 					<div class="list_new_view">
@@ -236,32 +233,5 @@ if (!is_search()) {
 		get_template_part('template-parts/entry-author-bio');
 	}
 	?>
-
-</div><!-- .section-inner -->
-
-<?php
-
-if (is_single()) {
-
-	get_template_part('template-parts/navigation');
-}
-
-/*
- * Output comments wrapper if it's a post, or if comments are open,
- * or if there's a comment number – and check for password.
- */
-if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
-?>
-
-	<div class="comments-wrapper section-inner">
-
-		<?php comments_template(); ?>
-
-	</div><!-- .comments-wrapper -->
-
-<?php
-}
-?>
-
 </article>
 <?php } ?>
