18aef19352a5bda39f196d6f94364ab7400fd0fe
Tan Le <19211tt1603@mail.tdc.edu.vn>
2021-11-11 20:03:45 +0700

    update

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -132,87 +132,91 @@ else{
 ?>
 
 <article <?php post_class($class); ?> id="post-<?php the_ID(); ?>">
-	<div class="header">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-8">
-					<?php
+<div class="row">
+	<div class="col-md-2">
+		<!--module 9-->
+	</div>
+	<div class="col-md-8">
+		<div class="header">
+				<div class="row">
+					<div class="col-md-8">
+						<?php
 
-					get_template_part( 'template-parts/entry-header' );
-					?>
-				</div>
-				<div class="col-md-2">
-					<div class="headlinesdate">
-						<div class="headlinesdm">
-							<div class="headlinesday">30</div>
-							<div class="headlinesmonth">09</div>
+						get_template_part( 'template-parts/entry-header' );
+						?>
+					</div>
+					<div class="col-md-2">
+						<div class="headlinesdate">
+							<div class="headlinesdm">
+								<div class="headlinesday">30</div>
+								<div class="headlinesmonth">09</div>
+							</div>
+							<div class="headlinesyear">'21</div>
 						</div>
-						<div class="headlinesyear">'21</div>
 					</div>
 				</div>
-			</div>
 		</div>
-	</div>
-		<?php
-		if ( ! is_search() ) {
-			get_template_part( 'template-parts/featured-image' );
-		}
+			<?php
+			if ( ! is_search() ) {
+				get_template_part( 'template-parts/featured-image' );
+			}
 
-		?>
-	<div class="gn">
-		<div class="container">
-		<div class="row">
-			<div class="col-md-12"><div class="overviewline"></div></div>
-	</div>
-    
-    <br>
-	<br>
+			?>
+		<div class="gn">
+			<div class="container">
+			<div class="row">
+				<div class="col-md-12"><div class="overviewline"></div></div>
+		</div>
+		
+		<br>
+		<br>
 
-        <div class="entry-content">
+			<div class="entry-content">
 
-            <?php
-				if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
-					the_excerpt();
-				} else {
-					if(is_single()){
-						the_content( __( 'Continue reading', 'twentytwenty' ) );
+				<?php
+					if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
+						the_excerpt();
+					} else {
+						if(is_single()){
+							the_content( __( 'Continue reading', 'twentytwenty' ) );
+						}
+						else{
+							$post = get_post();
+							echo substr($post -> post_content, 0, 150) . "[...]";
+						}
 					}
-					else{
-						$post = get_post();
-						echo substr($post -> post_content, 0, 150) . "[...]";
-					}
+					?>
+
+			</div><!-- .entry-content -->
+
+		</div><!-- .post-inner -->
+
+		<div class="section-inner">
+			<?php
+				wp_link_pages(
+					array(
+						'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
+						'after'       => '</nav>',
+						'link_before' => '<span class="page-number">',
+						'link_after'  => '</span>',
+					)
+				);
+
+				edit_post_link();
+
+				// Single bottom post meta.
+				twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
+
+				if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
+
+					get_template_part( 'template-parts/entry-author-bio' );
+
 				}
 				?>
 
-        </div><!-- .entry-content -->
-
-    </div><!-- .post-inner -->
-
-    <div class="section-inner">
-        <?php
-			wp_link_pages(
-				array(
-					'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
-					'after'       => '</nav>',
-					'link_before' => '<span class="page-number">',
-					'link_after'  => '</span>',
-				)
-			);
-
-			edit_post_link();
-
-			// Single bottom post meta.
-			twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
-
-			if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
-
-				get_template_part( 'template-parts/entry-author-bio' );
-
-			}
-			?>
-
-    </div><!-- .section-inner -->
-
+		</div><!-- .section-inner -->
+	</div>
+</div>
     <?php
 
 		if ( is_single() ) {
