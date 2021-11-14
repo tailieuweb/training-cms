b6e4543ce5217016e8d13911cc61feafe0f2d690
TranThiHoaiTrang <68778348+TranThiHoaiTrang@users.noreply.github.com>
2021-11-14 16:23:26 +0700

    update

---
+++ wp-content/plugins/woocommerce/packages/woocommerce-blocks/assets/js/types/type-defs/index.ts
@@ -0,0 +1,6 @@
+export * from './cart-response';
+export * from './product-response';
+export * from './cart';
+export * from './hooks';
+export * from './currency';
+export * from './payment-method-icon';
