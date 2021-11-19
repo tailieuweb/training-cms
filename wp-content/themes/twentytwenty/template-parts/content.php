981f5828aa661b7ff3214306d962dba1fc072c61
AnPhan <tlt2542001@gmail.com>
2021-11-19 08:00:49 +0700

    chinh sua

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -37,13 +37,8 @@
 			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
 				the_excerpt();
 			} else {
-				if (is_single()){
 					the_content( __( 'Continue reading', 'twentytwenty' ) );
-				}else {
-					$post = get_post();
-					echo substr($post->post_content, 0, 100);
-				}				
-			}
+				}			
 			?>
 
 		</div><!-- .entry-content -->
