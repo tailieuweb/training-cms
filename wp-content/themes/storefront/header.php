0d86cb04e5913443318ca5d9ed7f18b1b9fa441c
caotrunghieu1998 <caotrunghieu.tdc2019@gmail.com>
2021-11-29 17:50:00 +0700

    Add Bootstrap 4.0 and Font Awesome 4.7 for woocommerce

--- wp-content/themes/storefront/header.php
+++ wp-content/themes/storefront/header.php
@@ -48,6 +48,11 @@
 			 */
 			do_action('storefront_header');
 			?>
+			 <!-- Bootstrap 4 CSS -->
+			 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+			 integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+			 <!-- Font Awesome 4.7 -->
+			 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 			<!-- Import files custom css here -->
 			<?php for ($i = 1; $i <= 11; $i++) { ?>
 				<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css_file/module-' . $i . '-project.css' ?>" type="text/css" media="screen" />
