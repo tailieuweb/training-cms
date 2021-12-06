c482433463ef4a36a4121a18aa1ac347104e4214
NguyenMinhThien <nguyenminhthien263@gmail.com>
2021-12-06 15:58:38 +0700

    completed module 10 : footer

--- wp-content/themes/storefront/footer.php
+++ wp-content/themes/storefront/footer.php
@@ -9,32 +9,39 @@
 
 ?>
 
-		</div><!-- .col-full -->
-	</div><!-- #content -->
+</div><!-- .col-full -->
+</div><!-- #content -->
 
-	<?php do_action( 'storefront_before_footer' ); ?>
+<?php do_action('storefront_before_footer'); ?>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="col-full">
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<?php get_template_part('template-parts/footer-menus-widgets'); ?>
+	<div class="subfooter">
+		<div class="subfooter-copyright-text">
+			<p><span>&copy;</span> 2021 Group H. All rights reserved.</p>
+		</div>
+		<div class="subfooter-social-network-links">
+			<ul>
+				<li><a href="#">Fb.</a></li>
+				<li><a href="#">Tw.</a></li>
+				<li><a href="#">In.</a></li>
+				<li><a href="#">G<span>&#43</span>.</a></li>
+			</ul>
+		</div>
+	</div>
+</footer><!-- #colophon -->
 
-			<?php
-			/**
-			 * Functions hooked in to storefront_footer action
-			 *
-			 * @hooked storefront_footer_widgets - 10
-			 * @hooked storefront_credit         - 20
-			 */
-			do_action( 'storefront_footer' );
-			?>
-
-		</div><!-- .col-full -->
-	</footer><!-- #colophon -->
-
-	<?php do_action( 'storefront_after_footer' ); ?>
+<?php do_action('storefront_after_footer'); ?>
 
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+<script src="https://use.fontawesome.com/cabc80e9dd.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
 </html>
