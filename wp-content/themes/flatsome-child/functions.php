ca6d33c0a3949912a3d468c80c723058201520e7
ThanhAn <fxannguyen201@gmail.com>
2021-12-07 15:09:48 +0700

    Add File Css

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,8 +1,12 @@
 <?php
 // Add custom Theme Functions here
-add_action('wp_enqueue_scripts', 'enqueue_my_styles');
-function enqueue_my_styles()
+// ? Add file css
+function simplexml_basic_theme_load_scripts()
 {
-  wp_enqueue_style('my-theme-ie', get_stylesheet_directory_uri() . "/css/module5.css");
+  wp_enqueue_style('module5', get_stylesheet_directory_uri() . "/css/module5.css", array(), '1.0', 'all');
+  wp_enqueue_style('module4', get_stylesheet_directory_uri() . "/css/module4.css", array(), '1.0', 'all');
+  wp_enqueue_style('module3', get_stylesheet_directory_uri() . "/css/module3.css", array(), '1.0', 'all');
+  wp_enqueue_style('module2', get_stylesheet_directory_uri() . "/css/module2.css", array(), '1.0', 'all');
+  wp_enqueue_style('module1', get_stylesheet_directory_uri() . "/css/module1.css", array(), '1.0', 'all');
 }
-add_filter('woocommerce_show_page_title', '__return_false');
+add_action('wp_enqueue_scripts', 'simplexml_basic_theme_load_scripts');
