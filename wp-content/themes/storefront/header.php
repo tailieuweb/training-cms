60834163d32b3112890cd6812c4d2ef3cab09c63
Nguyễn Khắc Danh <80993371+NguyenKhacDanh@users.noreply.github.com>
2021-12-08 12:15:53 +0700

    save 12h15 8.12

--- wp-content/themes/storefront/header.php
+++ wp-content/themes/storefront/header.php
@@ -14,7 +14,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style-module3-woocommerce.css"
+        type="text/css" media="screen" />
 <?php wp_head(); ?>
 </head>
 
