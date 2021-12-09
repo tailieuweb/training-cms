31d715bafa3751dfd4ac96eea88cdd5a544ed66b
thanhquan1306 <19211tt0882@mail.tdc.edu.vn>
2021-12-09 22:19:36 +0700

    commit module 2

--- wp-content/themes/storefront/footer.php
+++ wp-content/themes/storefront/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer.
  *
@@ -9,28 +10,83 @@
 
 ?>
 
-		</div><!-- .col-full -->
-	</div><!-- #content -->
+</div><!-- .col-full -->
+</div><!-- #content -->
 
-	<?php do_action( 'storefront_before_footer' ); ?>
+<?php do_action('storefront_before_footer'); ?>
+<link rel="stylesheet" href="css_file/module-2-project.css">
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="col-full">
+	<div class="footer-holder" id="footer" data-id="footer">
+		<div class="jeg_footer jeg_footer_custom">
+			<div class="jeg_container">
+				<div class="jeg_content">
+					<div class="jeg_vc_content">
+						<div data-vc-full-width="true" data-vc-full-width-init="true" class="row vc_row vc_custom_1584292039669 vc_row-has-fill jnews_61b1c087c8c12 footer_light" style="position: relative; left: 0px; box-sizing: border-box; width: 1471px; padding-left: 0px; padding-right: 0px;">
+							<div class="jeg-vc-wrapper">
+								<div class="wpb_column jeg_column vc_column_container vc_col-sm-6">
+									<div class="jeg_wrapper wpb_wrapper">
+										<div class="wpb_text_column wpb_content_element ">
+											<div class="wpb_wrapper">
+												<p><span style="color: #ffffff;">TRƯỜNG CAO ĐẲNG CÔNG NGHỆ THỦ ĐỨC<br>KHOA CÔNG NGHỆ THÔNG TIN</span>
+													<span style="color: #ffffff;"><img src="https://ut.edu.vn/themes/ut/images/dia-chi.png" data-pin-no-hover="true">53 Võ Văn Ngân, Linh Chiểu, quận Thủ Đức, Thành phố Hồ Chí Minh</span>
+													<span style="color: #ffffff;"><img src="https://ut.edu.vn/themes/ut/images/di-dong.png" data-pin-no-hover="true"> <a style="color: #ffffff;" href="tel:0794821723"> 079.4821.723</a></span>
+													<span style="color: #ffffff;"><img src="https://ut.edu.vn/themes/ut/images/mail.png" data-pin-no-hover="true"> <a style="color: #ffffff;" href="caotrunghieu@gmail.com"> caotrunghieu@gmail.com</a></span>
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="wpb_column jeg_column vc_column_container vc_col-sm-6">
+									<div class="jeg_wrapper wpb_wrapper">
+										<div class="wpb_gmaps_widget wpb_content_element">
+											<div class="wpb_wrapper">
+												<div class="wpb_map_wraper"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2330.3016685788657!2d106.71694645784622!3d10.80482938988079!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7a7006b269783a40!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBHaWFvIHRow7RuZyBW4bqtbiB04bqjaSBUUC5IQ00!5e0!3m2!1sen!2s!4v1584280982150!5m2!1sen!2s" width="300" height="100" frameborder="0" style="border: 0px; pointer-events: none;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="vc_row-full-width vc_clearfix"></div>
+						<style type="text/css" scoped="">
+							.jnews_61b1c087c9d1c {
+								color: #ffffff
+							}
+						</style>
+						<div data-vc-full-width="true" data-vc-full-width-init="true" class="row vc_row vc_custom_1584291936147 vc_row-has-fill jnews_61b1c087c9d1c footer_light" style="position: relative; left: 0px; box-sizing: border-box; width: 1471px; padding-left: 0px; padding-right: 0px;">
+							<div class="jeg-vc-wrapper">
+								<div class="wpb_column jeg_column vc_column_container vc_col-sm-12">
+									<div class="jeg_wrapper wpb_wrapper"></div>
+								</div>
+							</div>
+						</div>
+						<div class="vc_row-full-width vc_clearfix"></div>
+						<style type="text/css" scoped="">
+							.jnews_61b1c087ca44e {
+								color: #ffffff
+							}
+						</style>
+						<div data-vc-full-width="true" data-vc-full-width-init="true" class="row vc_row vc_custom_1584291621248 vc_row-has-fill jnews_61b1c087ca44e footer_light" style="position: relative; left: 0px; box-sizing: border-box; width: 1471px; padding-left: 0px; padding-right: 0px;">
+							<div class="jeg-vc-wrapper">
+								<div class="wpb_column jeg_column vc_column_container vc_col-sm-12">
+									<div class="jeg_wrapper wpb_wrapper">
+										<div class="wpb_text_column wpb_content_element ">
+											<div class="wpb_wrapper">
+												<p style="text-align: center;">© Copyright 2021. Khoa Công nghệ thông tin</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="vc_row-full-width vc_clearfix"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-			<?php
-			/**
-			 * Functions hooked in to storefront_footer action
-			 *
-			 * @hooked storefront_footer_widgets - 10
-			 * @hooked storefront_credit         - 20
-			 */
-			do_action( 'storefront_footer' );
-			?>
-
-		</div><!-- .col-full -->
-	</footer><!-- #colophon -->
-
-	<?php do_action( 'storefront_after_footer' ); ?>
+<?php do_action('storefront_after_footer'); ?>
 
 </div><!-- #page -->
 
@@ -40,4 +96,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
+
 </html>
