be5b6023fc5571942b303713004337f7210461ce
thong <19211TT3039@mail.tdc.edu.vn>
2021-12-10 14:24:32 +0700

    update

--- wp-content/themes/storefront/footer.php
+++ wp-content/themes/storefront/footer.php
@@ -1,3 +1,15 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<link href="/your-path-to-fontawesome/css/all.css" rel="stylesheet"> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <?php
 /**
  * The template for displaying the footer.
@@ -17,16 +29,33 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 	
 		<div class="col-full dayne">
+		<div class="footer-widgets column-two grid-item">
+			
+			<div class="row">
+  <div class="col-sm-4"><?php dynamic_sidebar( 'sidebar-1' ); ?></div>
+  <div class="col-sm-8">
+	  <h2>Liên hệ</h2>
+ <div class="face">
+	 <a href="facebook.com"><i class="fab fa-facebook-square" style="color:3399FF;">facebook</i></a><br>
+ <a href=""><messenger class="com" ></messenger><i class="fab fa-facebook-messenger">messenger</i></a><br>
 
-			<?php
-			/**
-			 * Functions hooked in to storefront_footer action
-			 *
-			 * @hooked storefront_footer_widgets - 10
-			 * @hooked storefront_credit         - 20
-			 */
-			do_action( 'storefront_footer' );
-			?>
+ <a href="google.com"> <i class="fab fa-google-plus">google</i></a>
+
+ </div>
+
+ 
+ <p>địa chỉ 40/35 phước long b quận 9 thành phố HCM</p>
+ <p>sdt: <span>031231231231</span></p>
+
+</div>
+
+</div>
+<div class"footer1" style="text-align:center;" >
+	<h3>ShopSale</h3>
+	<p>&copy; 2021 Công ty cổ phần sale  </p>
+</div>
+
+		
 
 		</div><!-- .col-full -->
 	</footer><!-- #colophon -->
