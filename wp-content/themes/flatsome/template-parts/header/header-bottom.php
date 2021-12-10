292e67840bc465d33a7a72cf84f0e72f04b13bfe
NguyenThanhTam <ntamberyl@gmail.com>
2021-12-10 16:27:32 +0700

    updates module1

--- wp-content/themes/flatsome/template-parts/header/header-bottom.php
+++ wp-content/themes/flatsome/template-parts/header/header-bottom.php
@@ -13,8 +13,24 @@
 
             <?php if(get_theme_mod('header_elements_bottom_center')){ ?>
             <div class="flex-col hide-for-medium flex-center">
-                <ul class="nav header-nav header-bottom-nav nav-center <?php flatsome_nav_classes('bottom'); ?>">
+                <!-- <ul class="nav header-nav header-bottom-nav nav-center <?php flatsome_nav_classes('bottom'); ?>">
                     <?php flatsome_header_elements('header_elements_bottom_center','nav_position_text'); ?>
+                </ul> -->
+                <ul class="nav header-nav header-bottom-nav nav-center  nav-box nav-uppercase">
+                    <li id="menu-item-435" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-435 menu-item-design-default has-dropdown"><a href="http://wordpress.local/?page_id=428" class="nav-top-link"><i class="fas fa-bars"></i> Danh mục sản phẩm</a>
+                        <ul class="sub-menu nav-dropdown nav-dropdown-simple">
+                            <li id="menu-item-436" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-436"><a href="http://wordpress.local/?product_cat=dieu-hoa">Điều hòa Funiki</a></li>
+                            <li id="menu-item-437" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-437"><a href="http://wordpress.local/?product_cat=tu-lanh">Tủ đông</a></li>
+                            <li id="menu-item-439" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-439"><a href="http://wordpress.local/?product_cat=san-pham-xem-nhieu">Sản phẩm xem nhiều</a></li>
+                        </ul>
+                    </li>
+                    <li id="menu-item-429" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-65 current_page_item menu-item-429 active menu-item-design-default"><a href="http://wordpress.local/" aria-current="page" class="nav-top-link">Trang chủ</a></li>
+                    <li id="menu-item-430" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-430 menu-item-design-default"><a href="http://wordpress.local/?page_id=367" class="nav-top-link">Giới thiệu</a></li>
+                    <li id="menu-item-431" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-431 menu-item-design-default"><a href="http://wordpress.local/?page_id=8" class="nav-top-link">Cửa hàng</a></li>
+                    <li id="menu-item-432" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-432 menu-item-design-default"><a href="http://wordpress.local/?page_id=377" class="nav-top-link">Bảo hành</a></li>
+                    <li id="menu-item-433" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-433 menu-item-design-default"><a href="http://wordpress.local/?page_id=369" class="nav-top-link">Tin tức</a></li>
+                    <li id="menu-item-434" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-434 menu-item-design-default"><a href="http://wordpress.local/?page_id=373" class="nav-top-link">Liên hệ</a></li>
+                    <!-- <li class="html custom html_nav_position_text"><a href="#">KHUYẾN MÃI GIỜ VÀNG</a></li> -->
                 </ul>
             </div>
             <?php } ?>
--- wp-content/themes/flatsome/template-parts/header/header-main.php
+++ wp-content/themes/flatsome/template-parts/header/header-main.php
@@ -23,8 +23,48 @@
 
           <!-- Right Elements -->
           <div class="flex-col hide-for-medium flex-right">
-            <ul class="header-nav header-nav-main nav nav-right <?php flatsome_nav_classes('main'); ?>">
+            <!-- <ul class="header-nav header-nav-main nav nav-right <?php flatsome_nav_classes('main'); ?>">
               <?php flatsome_header_elements('header_elements_right'); ?>
+            </ul> -->
+            <ul class="header-nav header-nav-main nav nav-right  nav-uppercase">
+              <li class="html custom html_topbar_right">
+                <div class="list-icon-text">
+	                <div class="icon-box">
+	                  <i class="fas fa-phone"></i>
+	                </div>
+                  <div class="text-box">
+                    <span class="text-banhang" style="color:#1e428a">BÁN HÀNG</span><br>
+                    <strong class="hotline" style="color:red">0986.777.888</strong>
+                  </div>
+                </div>
+              </li>
+              <li class="html custom html_top_right_text">
+                <div class="list-icon-text">
+                  <div class="icon-box">
+                    <i class="fas fa-phone"></i>
+                  </div>
+                  <div class="text-box">
+                    <span class="text-kythuat" style="color:#1e428a">KỸ THUẬT</span><br>
+                    <strong class="hotline" style="color:red">0986.777.888</strong>
+                  </div>
+                </div>
+              </li>
+              <li class="html custom html_nav_position_text_top">
+                <span class="text-giohang" style="color:#777"> GIỎ HÀNG </span>
+                <li class="cart-item has-icon has-dropdown">
+                  <a href="http://wordpress.local/?page_id=9" title="Cart" class="header-cart-link is-small">
+                    <span class="cart-icon image-icon">
+			                <strong>0</strong>
+		                </span>
+                  </a>
+                  <ul class="nav-dropdown nav-dropdown-simple">
+                    <li class="html widget_shopping_cart">
+                      <div class="widget_shopping_cart_content">
+                        <p class="woocommerce-mini-cart__empty-message">No products in the cart.</p>
+                      </div>
+                    </li>
+                  </ul>
+                </li>
             </ul>
           </div>
 
