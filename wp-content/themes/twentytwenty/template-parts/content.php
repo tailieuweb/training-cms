1b99e8aef4d5dbcc02411cc26898f0362415cf46
ngohieu96 <ngohieu.edu@gmail.com>
2021-12-11 06:26:35 +0700

    update

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -462,44 +462,43 @@ if (is_search()) {
 					</div>
 					<div class="col-md-3">
 						<div class="rp-box">
-							<h2>Comment</h2>
-							<p>Hello</p>
-							<p>asdada</p>
+							<h2>Comment</h2>						
 							<div class="crossedbg"></div>
 							<?php dynamic_sidebar('sidebar-4'); ?>
-
+							<p>Hello</p>
+							<p>asdada</p>
 						</div>
 					</div>
 				</div>
-				
+
 			</article><!-- .post -->
 		</div>
 	</div>
 	<?php
 
-							if (is_single()) {
+	if (is_single()) {
 
-								get_template_part('template-parts/navigation');
-							}
+		get_template_part('template-parts/navigation');
+	}
 
-							/*
+	/*
 		* Output comments wrapper if it's a post, or if comments are open,
 		* or if there's a comment number – and check for password.
 		*/
-							?>
+	?>
 
 	<?php
-				if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
-				?>
+	if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
+	?>
 
-					<div class="comments-wrapper section-inner">
+		<div class="comments-wrapper section-inner">
 
-						<?php comments_template(); ?>
+			<?php comments_template(); ?>
 
-					</div><!-- .comments-wrapper -->
+		</div><!-- .comments-wrapper -->
 
-				<?php
-				}
-				?>
+	<?php
+	}
+	?>
 	<hr class="hr">
 <?php } ?>
