3c9661220d985d82f5b1460c9b3566c417f03c32
Nguyen Trong Tinh <trongtinh2k@gmail.com>
2021-12-10 10:58:26 +0700

    commit

---
+++ wp-content/plugins/google-listings-and-ads/js/build/custom-inputs.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '81da147e46148471fc815506ba8768e6');
---
+++ wp-content/plugins/mailpoet/vendor/autoload.php
@@ -0,0 +1,2 @@
+<?php
+ if (!defined('ABSPATH')) exit; require_once __DIR__ . '/composer/autoload_real.php'; return ComposerAutoloaderInitfbeebd745e186f2001cf5818dfb9a826::getLoader(); 
