d97b7634e3507b1b7492c1505e855b4173a49350
Hoang Vu <phanvu000666@gmail.com>
2021-12-11 11:16:08 +0700

    .

--- wp-content/themes/storefront/header.php
+++ wp-content/themes/storefront/header.php
@@ -14,7 +14,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/mycss6.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/mycss567.css" type="text/css" media="screen" />
 <?php wp_head(); ?>
 </head>
 
